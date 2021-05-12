7b759255b88a7057c9d100c8ccbb5c02d28752dc
evgeny pavlov <epavlov@mozilla.com>
2021-05-11 17:21:06 -0700

    Fix install scripts

--- config.sh
+++ config.sh
@@ -6,15 +6,15 @@ MARIAN=${WORKDIR}/marian-dev/build
 CLEAN_TOOLS=${WORKDIR}/students/train-student/clean/tools
 TEACHER_PATH=${MODELS_DIR}/teacher
 
-SRC=es
+SRC=ru
 TRG=en
 TRAIN_DATASETS=OPUS_Europarl_v8
-DEVTEST_DATASETS=newstest2013 newstest2012 newstest2011 newstest2010
-TEST_DATASETS=newstest2013 newstest2012 newstest2011 newstest2010
-MONO_DATASETS=wmt20 wmt16 wmt14 wmt13
+DEVTEST_DATASETS="newstest2013 newstest2012 newstest2011 newstest2010"
+TEST_DATASETS="wmt20 wmt18 wmt16 wmt15"
+MONO_DATASETS=
 MONO_MAX_SENTENCES=200000000
 
 
 # marian --devices parameter for GPUs to use, for example 0 1 2 3
-GPUS=$(seq -s " " 0 ($(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)-1))
+GPUS=$(seq -s " " 0 $(( $(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)-1 )))
 WORKSPACE=14000
--- pipeline/0-setup/install.sh
+++ pipeline/0-setup/install.sh
@@ -18,14 +18,22 @@ echo "Installing Intel MKL"
 wget -qO- 'https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB' | sudo apt-key add -
 sudo sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
 sudo apt-get update
-sudo apt-get install intel-mkl-64bit-2020.0-088
+sudo apt-get install -y intel-mkl-64bit-2020.0-088
+
+echo "Installing CMake"
+sudo apt-get install -y \
+    autoconf \
+    automake
+wget https://github.com/Kitware/CMake/releases/download/v3.20.2/cmake-3.20.2-Linux-x86_64.sh
+sudo chmod +x cmake-3.20.2-Linux-x86_64.sh
+sudo ./cmake-3.20.2-Linux-x86_64.sh --skip-license --prefix=/usr/local
 
 
 echo "Compiling marian-dev"
 if [ ! -e ${WORKDIR}/marian-dev/build/marian ]; then
   mkdir -p ${WORKDIR}/marian-dev/build
   cd marian-dev/build
-  cmake .. -DUSE_SENTENCEPIECE=on -DUSE_FBGEMM=on -DCOMPILE_CPU=on -DCMAKE_BUILD_TYPE=Release \
+  /usr/local/bin/cmake .. -DUSE_SENTENCEPIECE=on -DUSE_FBGEMM=on -DCOMPILE_CPU=on -DCMAKE_BUILD_TYPE=Release \
             -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_DIR}
   make -j$(nproc)
   cd ../..
@@ -38,7 +46,7 @@ sudo apt-get install -y python3 python3-venv python3-pip
 pip3 install virtualenv
 python3 -m venv bergamot-training-venv
 source bergamot-training-venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install -r ${WORKDIR}/pipeline/0-setup/requirements.txt
 
 
 echo "Installing fast_align dependencies"
