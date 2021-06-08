c4b0cc1095a8873ffd334428defed673334db124
evgeny pavlov <epavlov@mozilla.com>
2021-06-07 18:29:24 -0700

    Fix export

--- pipeline/quantize/export.sh
+++ pipeline/quantize/export.sh
@@ -11,22 +11,32 @@ set -euo pipefail
 
 test -v SRC
 test -v TRG
+test -v MARIAN
 
 model_dir=${1}
 shortlist=${2}
 output_dir=${3}
 
-cp "${model_dir}/model.intgemm.alphas.bin" "${output_dir}/model.${SRC}${TRG}.intgemm.alphas.bin"
-pigz "${output_dir}/model.${SRC}${TRG}.intgemm.alphas.bin"
+mkdir -p "${output_dir}"
 
-cp "${shortlist}" "${output_dir}/lex.50.50.${SRC}${TRG}.s2t.bin.gz"
+echo "### Exporting quantized model"
+model="${output_dir}/model.${SRC}${TRG}.intgemm.alphas.bin"
+cp "${model_dir}/model.intgemm.alphas.bin" "${model}"
+pigz "${model}"
 
-cp "${model_dir}/vocab.spm" "${output_dir}/vocab.${SRC}${TRG}.spm"
-pigz "${output_dir}/vocab.${SRC}${TRG}.spm"
+shortlist_bin="${output_dir}/lex.50.50.${SRC}${TRG}.s2t.bin"
+"${MARIAN}"/marian-conv \
+  --shortlist "${shortlist}" 50 50 0 \
+  --dump "${shortlist_bin}" \
+  --vocabs "${model_dir}/vocab.spm" "${model_dir}/vocab.spm"
+pigz "${shortlist_bin}"
 
+vocab="${output_dir}/vocab.${SRC}${TRG}.spm"
+cp "${model_dir}/vocab.spm" "${vocab}"
+pigz "${vocab}"
 
 test -s "${output_dir}/model.${SRC}${TRG}.intgemm.alphas.bin.gz" || exit 1
 test -s "${output_dir}/lex.50.50.${SRC}${TRG}.s2t.bin.gz" || exit 1
 test -s "${output_dir}/vocab.${SRC}${TRG}.spm.gz" || exit 1
 
-
+echo "### Export is completed. Results: ${output_dir}"
