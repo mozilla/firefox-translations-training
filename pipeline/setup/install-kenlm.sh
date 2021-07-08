295a77f7c7c7f8dd07994c0d0b2630012453049c
evgeny pavlov <epavlov@mozilla.com>
2021-07-08 14:29:56 -0700

    Always install kenlm with pip

--- pipeline/setup/install-kenlm.sh
+++ pipeline/setup/install-kenlm.sh
@@ -13,17 +13,18 @@ echo "###### Installing kenlm"
 test -v WORKDIR
 test -v BIN
 
-if [ ! -e "${BIN}/kenlm" ]; then
-  cd "${WORKDIR}/3rd_party/kenlm"
-  source "${WORKDIR}/pipeline/setup/activate-python.sh"
-  python -m pip install . --install-option="--max_order 7"
+cd "${WORKDIR}/3rd_party/kenlm"
 
+if [ ! -e "${BIN}/kenlm" ]; then
   mkdir -p build
   cd build
   mkdir "${BIN}/kenlm"
   cmake .. -DKENLM_MAX_ORDER=7 -DCMAKE_INSTALL_PREFIX:PATH="${BIN}/kenlm"
   make -j all install
-  cd "${WORKDIR}"
 fi
+
+source "${WORKDIR}/pipeline/setup/activate-python.sh"
+python -m pip install . --install-option="--max_order 7"
+cd "${WORKDIR}"
 
 echo "###### Done: Installing kenlm"
