9eae67e50b0a67ce56aa3b9539f2818716b5551c
evgeny pavlov <epavlov@mozilla.com>
2021-07-08 14:36:18 -0700

    Install miniconda to bin folder

--- pipeline/setup/activate-python.sh
+++ pipeline/setup/activate-python.sh
@@ -6,6 +6,6 @@
 #   source ./activate-python.sh
 #
 
-PATH="/root/miniconda3/bin:${PATH}"
-source /root/miniconda3/etc/profile.d/conda.sh
+PATH="${BIN}/miniconda3/bin:${PATH}"
+source "${BIN}/miniconda3/etc/profile.d/conda.sh"
 conda activate bergamot-training-env
--- pipeline/setup/install-python.sh
+++ pipeline/setup/install-python.sh
@@ -11,12 +11,12 @@ set -euo pipefail
 
 echo "###### Installing Python"
 
-if [ ! -e /root/miniconda3/bin/conda ]; then
+if [ ! -e "${BIN}/miniconda3/bin/conda" ]; then
   wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-  bash ./Miniconda3-latest-Linux-x86_64.sh -b -u
+  bash ./Miniconda3-latest-Linux-x86_64.sh -b -u -p "${BIN}/miniconda3"
   rm -f Miniconda3-latest-Linux-x86_64.sh
 fi
-export PATH="/root/miniconda3/bin:${PATH}"
+export PATH="${BIN}/miniconda3/bin:${PATH}"
 conda create -y --name bergamot-training-env python=3.8
 
 
