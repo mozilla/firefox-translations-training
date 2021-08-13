ca83f89a4d1dc34385522fadb6b63fce2c3430a3
evgeny pavlov <epavlov@mozilla.com>
2021-08-12 17:30:45 -0700

    Add flores importer

---
+++ pipeline/data/importers/corpus/flores.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+##
+# Downloads flores dataset
+# Dataset type can be "dev" or "devtest"
+#
+# Usage:
+#   bash flores.sh source target dir dataset
+#
+
+set -x
+set -euo pipefail
+
+echo "###### Downloading flores corpus"
+
+src=$1
+trg=$2
+dir=$3
+dataset=$4
+
+tmp="${dir}/flores"
+mkdir -p "${tmp}"
+
+test -s "${tmp}/flores101_dataset.tar.gz" ||
+  wget -O "${tmp}/flores101_dataset.tar.gz" "https://dl.fbaipublicfiles.com/flores101/dataset/flores101_dataset.tar.gz"
+
+tar -xzf "${tmp}/flores101_dataset.tar.gz"
+
+source "${WORKDIR}/pipeline/setup/activate-python.sh"
+
+trg_flores=$(python -c "from mtdata.iso import iso3_code; print(iso3_code('${trg}', fail_error=True))")
+
+flores_code() {
+  code=$1
+
+  if [ "${code}" == "zh" ] || [ "${code}" == "zh-Hans" ]; then
+    flores_code="zho_simpl"
+  elif [ "${code}" == "zh-Hant" ]; then
+    flores_code="zho_trad"
+  else
+    flores_code=$(python -c "from mtdata.iso import iso3_code; print(iso3_code('${code}', fail_error=True))")
+  fi
+
+  echo "${flores_code}"
+}
+
+src_flores=$(flores_code "${src}")
+trg_flores=$(flores_code "${trg}")
+
+pigz -c "${tmp}/flores101_dataset/${dataset}/${src_flores}.${dataset}" >"${dir}/flores.${src}"
+pigz -c "${tmp}/flores101_dataset/${dataset}/${trg_flores}.${dataset}" >"${dir}/flores.${trg}"
+
+rm -rf "${tmp}"
+
+echo "###### Done: Downloading flores corpus"
