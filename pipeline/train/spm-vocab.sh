cc28d6e5f9a3ad5af8924ab308096fafe4a07628
evgeny pavlov <epavlov@mozilla.com>
2021-10-20 14:19:13 -0700

    Reduce sample size for vocab

--- pipeline/train/spm-vocab.sh
+++ pipeline/train/spm-vocab.sh
@@ -18,8 +18,8 @@ vocab_output=$3
 vocab_dir=$(dirname "${vocab_output}")
 mkdir -p "${vocab_dir}"
 
-pigz -dc "${corpus_src}" | shuf -n 20000000 >"${vocab_dir}/data.txt"
-pigz -dc "${corpus_trg}" | shuf -n 20000000 >>"${vocab_dir}/data.txt"
+pigz -dc "${corpus_src}" | shuf -n 10000000 >"${vocab_dir}/data.txt"
+pigz -dc "${corpus_trg}" | shuf -n 10000000 >>"${vocab_dir}/data.txt"
 
 "${MARIAN}/spm_train" --bos_id=-1 --eos_id=0 --unk_id=1 --user_defined_symbols="" \
   --input="${vocab_dir}/data.txt" --model_prefix="${vocab_dir}/vocab" --vocab_size=32000
