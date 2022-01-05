440ce4d95154b01925cd40257adc6ae744799308
evgeny pavlov <epavlov@mozilla.com>
2022-01-05 13:16:35 -0800

    Do not concatenate vocab corpus

--- pipeline/train/spm-vocab.sh
+++ pipeline/train/spm-vocab.sh
@@ -16,14 +16,14 @@ sample_size=$4
 vocab_dir=$(dirname "${vocab_output}")
 mkdir -p "${vocab_dir}"
 
-pigz -dc "${corpus_src}" >"${vocab_dir}/data.txt"
-pigz -dc "${corpus_trg}" >>"${vocab_dir}/data.txt"
+pigz -dc "${corpus_src}" >"${vocab_dir}/data.src.txt"
+pigz -dc "${corpus_trg}" >"${vocab_dir}/data.trg.txt"
 
 "${MARIAN}/spm_train" --bos_id=-1 --eos_id=0 --unk_id=1 --user_defined_symbols="" \
   --model_prefix="${vocab_dir}/vocab" --vocab_size=32000 \
-  --input="${vocab_dir}/data.txt" \
+  --input="${vocab_dir}/data.src.txt,${vocab_dir}/data.trg.txt" \
   --input_sentence_size="${sample_size}" --shuffle_input_sentence=true
 
-rm "${vocab_dir}/data.txt"
+rm "${vocab_dir}/data.src.txt" "${vocab_dir}/data.trg.txt"
 
 mv "${vocab_dir}/vocab.model" "${vocab_output}"
