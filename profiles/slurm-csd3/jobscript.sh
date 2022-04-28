bb786281dc9366a03287dcf2092d6c9fe81cdcec
evgeny pavlov <epavlov@mozilla.com>
2022-04-27 18:19:34 -0700

    Fix jobscript

--- profiles/slurm-csd3/jobscript.sh
+++ profiles/slurm-csd3/jobscript.sh
@@ -3,9 +3,34 @@
 
 export SINGULARITYENV_CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
 
-{exec_job}
+if [ ! -z "$SLURM_ARRAY_JOB_ID" ]; then
+    trap 'kill "$jobpid"' TERM
+    trap 'kill "$jobpid"' INT
+    trap 'kill "$jobpid"' USR1
 
-# for job arrays
-exit_status=$?
-scancel $SLURM_JOBID
-exit ${exec_exit_status}
+    {exec_job} &
+    jobpid=$!
+
+    wait $jobpid
+    wait $jobpid
+    exit_status=$?
+
+    # Now we interpret the exit status.
+    if [ "$exit_status" == 143 ]; then # 143 = 128 + 15 => SIGTERM
+        echo "Not done, continue in the next slot"
+    else
+        if [ "$exit_status" == 0 ]; then
+            echo "Completed"
+        else
+            echo "Failed"
+        fi;
+        # Either we are finished or training failed so we need to
+        # investigate; there's no point in continuing training here.
+        echo "Cancelling job array"
+        scancel --state=PENDING $SLURM_ARRAY_JOB_ID
+    fi
+
+    exit $exit_status
+else
+    {exec_job}
+fi
