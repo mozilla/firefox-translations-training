4e6d3bc6d3348a7740831d999183f4466111aec7
TommiNieminen <nieminentommi@hotmail.com>
2023-03-17 10:50:39 +0200

    fixed data language pair reverse with tatoeba data

--- pipeline/data/download-tc-data.sh
+++ pipeline/data/download-tc-data.sh
@@ -21,19 +21,25 @@ mkdir -p "${tmp}"
 archive_path="${tmp}/${version}-${src_three_letter}-${trg_three_letter}.tar"
 
 #try both combinations of language codes 
-wget -O "${archive_path}" "https://object.pouta.csc.fi/${version}/${src_three_letter}-${trg_three_letter}.tar" || wget -O "${archive_path}" "https://object.pouta.csc.fi/${version}/${trg_three_letter}-${src_three_letter}.tar"
+if wget -O "${archive_path}" "https://object.pouta.csc.fi/${version}/${src_three_letter}-${trg_three_letter}.tar"; then
+   package_src=${src}
+   package_trg=${trg} 
+elif wget -O "${archive_path}" "https://object.pouta.csc.fi/${version}/${trg_three_letter}-${src_three_letter}.tar"; then
+   package_src=${trg}
+   package_trg=${src}
+fi
 
 #extract all in same directory, saves the trouble of parsing directory structure
 tar -xf "${archive_path}" --directory ${tmp} --strip-components 4 
 
-mv ${tmp}/train.src.gz ${output_prefix}/corpus/tc_${version}.${src}.gz
-mv ${tmp}/train.trg.gz ${output_prefix}/corpus/tc_${version}.${trg}.gz
+mv ${tmp}/train.src.gz ${output_prefix}/corpus/tc_${version}.${package_src}.gz
+mv ${tmp}/train.trg.gz ${output_prefix}/corpus/tc_${version}.${package_trg}.gz
 
-cat ${tmp}/dev.src | gzip > ${output_prefix}/devset/tc_${version}.${src}.gz
-cat ${tmp}/dev.trg | gzip > ${output_prefix}/devset/tc_${version}.${trg}.gz
+cat ${tmp}/dev.src | gzip > ${output_prefix}/devset/tc_${version}.${package_src}.gz
+cat ${tmp}/dev.trg | gzip > ${output_prefix}/devset/tc_${version}.${package_trg}.gz
 
-cat ${tmp}/test.src | gzip > ${output_prefix}/eval/tc_${version}.${src}.gz
-cat ${tmp}/test.trg | gzip > ${output_prefix}/eval/tc_${version}.${trg}.gz
+cat ${tmp}/test.src | gzip > ${output_prefix}/eval/tc_${version}.${package_src}.gz
+cat ${tmp}/test.trg | gzip > ${output_prefix}/eval/tc_${version}.${package_trg}.gz
 
 rm -rf "${tmp}"
 
