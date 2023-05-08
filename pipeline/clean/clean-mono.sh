3735c98cf369ea1ca2d3e7e8d8c64ab29a76ab22
Ben Hearsum <ben@mozilla.com>
2023-05-08 16:43:14 -0400

    Update clean-mono.sh to use abstracted compression commands and artifact extensions

--- pipeline/clean/clean-mono.sh
+++ pipeline/clean/clean-mono.sh
@@ -14,8 +14,8 @@ output_prefix=$3
 threads=$4
 dataset=$5
 
-COMPRESSION_CMD="${COMPRESSION_CMD:-pigz}"
-ARTIFACT_EXT="${ARTIFACT_EXT:-gz}"
+COMPRESSION_CMD="${COMPRESSION_CMD:-${COMPRESSION_CMD}}"
+ARTIFACT_EXT="${ARTIFACT_EXT:-${ARTIFACT_EXT}}"
 
 echo "### Cleaning ${input_prefix}"
 
@@ -27,47 +27,47 @@ mkdir -p "${dir}"
 
 ######################################################################
 echo "### Basic preprocessing"
-test -s "${output_prefix}.${lang}.nrm.gz" ||
-  zstdmt -dc "${input_prefix}.${lang}.zst" |
+test -s "${output_prefix}.${lang}.nrm.${ARTIFACT_EXT}" ||
+  ${COMPRESSION_CMD} -dc "${input_prefix}.${lang}.${ARTIFACT_EXT}" |
   parallel --no-notice --pipe -k -j "${threads}" --block 50M \
     "perl tools/deescape-special-chars.perl | perl tools/remove-non-printing-char.perl" |
-  zstdmt -c >"${output_prefix}.${lang}.nrm.zst"
+  ${COMPRESSION_CMD} -c >"${output_prefix}.${lang}.nrm.${ARTIFACT_EXT}"
 
 #####################################################################
 echo "### Apply monolingual fixes"
 if [[ ! -x fixes/${dataset}.${lang}.sh ]]; then
-  test -s "${output_prefix}.${lang}.monofix.gz" ||
-    cp "${output_prefix}.${lang}.nrm.gz" "${output_prefix}.${lang}.monofix.gz"
+  test -s "${output_prefix}.${lang}.monofix.${ARTIFACT_EXT}" ||
+    cp "${output_prefix}.${lang}.nrm.${ARTIFACT_EXT}" "${output_prefix}.${lang}.monofix.${ARTIFACT_EXT}"
 else
-  test -s "${output_prefix}.${lang}.monofix.gz" ||
-    pigz -dc "${output_prefix}.${lang}.nrm.gz" \
+  test -s "${output_prefix}.${lang}.monofix.${ARTIFACT_EXT}" ||
+    ${COMPRESSION_CMD} -dc "${output_prefix}.${lang}.nrm.${ARTIFACT_EXT}" \
         | fixes/"${dataset}"."${lang}".sh \
-        | pigz >"${output_prefix}.${lang}.monofix.gz"
+        | ${COMPRESSION_CMD} >"${output_prefix}.${lang}.monofix.${ARTIFACT_EXT}"
 fi
 
 ######################################################################
 echo "### Language identification"
-test -s "${output_prefix}.${lang}.langid.gz" ||
-  pigz -dc "${output_prefix}.${lang}.monofix.gz" |
+test -s "${output_prefix}.${lang}.langid.${ARTIFACT_EXT}" ||
+  ${COMPRESSION_CMD} -dc "${output_prefix}.${lang}.monofix.${ARTIFACT_EXT}" |
   # memory intensive
   parallel --no-notice --pipe -k -j "$(echo "${threads}"/4 | bc)" --block 50M "python tools/langid_fasttext.py" |
   grep -P "^${lang}\t" | cut -f2 |
-  pigz >"${output_prefix}.${lang}.langid.gz"
+  ${COMPRESSION_CMD} >"${output_prefix}.${lang}.langid.${ARTIFACT_EXT}"
 
 ######################################################################
 echo "### Rule-based filtering"
 
-pigz -dc "${output_prefix}.${lang}.langid.gz" |
+${COMPRESSION_CMD} -dc "${output_prefix}.${lang}.langid.${ARTIFACT_EXT}" |
 parallel --no-notice --pipe -k -j "${threads}" --block 50M \
   "python tools/clean_mono.py -l ${lang} --debug" \
   2>"${output_prefix}.${lang}.clean.debug.txt" |
-pigz >"${output_prefix}.${lang}.gz"
+${COMPRESSION_CMD} >"${output_prefix}.${lang}.${ARTIFACT_EXT}"
 
-test -s "${output_prefix}.${lang}.gz" || exit 1
+test -s "${output_prefix}.${lang}.${ARTIFACT_EXT}" || exit 1
 
 echo "### Remove data from intermediate steps"
-rm -rf "${output_prefix}".*.nrm.gz "${output_prefix}".*.langid.gz \
-  "${output_prefix}".*.monofix.gz
+rm -rf "${output_prefix}".*.nrm.${ARTIFACT_EXT} "${output_prefix}".*.langid.${ARTIFACT_EXT} \
+  "${output_prefix}".*.monofix.${ARTIFACT_EXT}
 
 echo "### Clean data is written to  ${output_prefix}"
 
