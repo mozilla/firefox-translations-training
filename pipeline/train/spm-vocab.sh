cddb9529485780d6d6558801e289ff2757edaaa1
Ben Hearsum (he/him) <ben@mozilla.com>
2023-05-24 09:30:08 -0400

    Add support for `train-vocab` (#123)
    * Support zstd & overriding number of threads in spm-vocab.sh
    
    * Fix parameters to merge corpus script
    
    I discovered that these artifacts are not actually published right now. The pipeline script wants a prefix, not a directory.
    
    * Add train-vocab kind
    
    As with the previous `merge-corpus`, this is a pretty straightforward step. It's solely downstream of `merge-corpus`, and takes one parameter from the action input.

--- pipeline/train/spm-vocab.sh
+++ pipeline/train/spm-vocab.sh
@@ -12,17 +12,25 @@ corpus_src=$1
 corpus_trg=$2
 vocab_output=$3
 sample_size=$4
+threads=$5
+
+if [ "$threads" = "auto" ]; then
+  threads=$(nproc)
+fi
+
+COMPRESSION_CMD="${COMPRESSION_CMD:-pigz}"
 
 vocab_dir=$(dirname "${vocab_output}")
 mkdir -p "${vocab_dir}"
 
-pigz -dc "${corpus_src}" >"${vocab_dir}/data.src.txt"
-pigz -dc "${corpus_trg}" >"${vocab_dir}/data.trg.txt"
+${COMPRESSION_CMD} -dc "${corpus_src}" >"${vocab_dir}/data.src.txt"
+${COMPRESSION_CMD} -dc "${corpus_trg}" >"${vocab_dir}/data.trg.txt"
 
 "${MARIAN}/spm_train" --bos_id=-1 --eos_id=0 --unk_id=1 --user_defined_symbols="" \
   --model_prefix="${vocab_dir}/vocab" --vocab_size=32000 \
   --input="${vocab_dir}/data.src.txt,${vocab_dir}/data.trg.txt" \
-  --input_sentence_size="${sample_size}" --shuffle_input_sentence=true
+  --input_sentence_size="${sample_size}" --shuffle_input_sentence=true \
+  --num_threads "${threads}"
 
 rm "${vocab_dir}/data.src.txt" "${vocab_dir}/data.trg.txt"
 
