9aed3747f95e8f48c4f4dbbfed10ce4bd7d31fb8
Ben Hearsum <ben@mozilla.com>
2023-05-24 15:53:53 -0400

    Support zstd in train.sh pipeline script
    Unlike previous pipeline scripts we've worked with, this one can't (AFAICT) take the input through a pipe, and marian doesn't support decoding zstd files. Instead, we simply decompress all of the input files, and let marian have them in plaintext format instead. (This is skipped for gz, to avoid modifying the existing pipeline.)

--- pipeline/train/train.sh
+++ pipeline/train/train.sh
@@ -19,6 +19,8 @@ vocab=$8
 best_model_metric=$9
 extra_params=( "${@:10}" )
 
+ARTIFACT_EXT="${ARTIFACT_EXT:-gz}"
+
 test -v GPUS
 test -v MARIAN
 test -v WORKSPACE
@@ -32,10 +34,22 @@ echo "### Training ${model_dir}"
 
 # if doesn't fit in RAM, remove --shuffle-in-ram and add --shuffle batches
 
+# Marian doesn't support zst natively; we need to decompress before passing them
+# along.
+if [ "${ARTIFACT_EXT}" = "zst" ]; then
+  zstdmt --rm -d "${train_set_prefix}.${src}.${ARTIFACT_EXT}"
+  zstdmt --rm -d "${train_set_prefix}.${trg}.${ARTIFACT_EXT}"
+  zstdmt --rm -d "${valid_set_prefix}.${src}.${ARTIFACT_EXT}"
+  zstdmt --rm -d "${valid_set_prefix}.${trg}.${ARTIFACT_EXT}"
+  ARTIFACT_EXT=""
+else
+  ARTIFACT_EXT=".gz"
+fi
+
 "${MARIAN}/marian" \
   --model "${model_dir}/model.npz" \
   -c "configs/model/${model_type}.yml" "configs/training/${model_type}.${training_type}.yml" \
-  --train-sets "${train_set_prefix}".{"${src}","${trg}"}.gz \
+  --train-sets "${train_set_prefix}".{"${src}","${trg}"}${ARTIFACT_EXT} \
   -T "${model_dir}/tmp" \
   --shuffle-in-ram \
   --vocabs "${vocab}" "${vocab}" \
@@ -44,7 +58,7 @@ echo "### Training ${model_dir}"
   --sharding local \
   --sync-sgd \
   --valid-metrics "${best_model_metric}" ${all_model_metrics[@]/$best_model_metric} \
-  --valid-sets "${valid_set_prefix}".{"${src}","${trg}"}.gz \
+  --valid-sets "${valid_set_prefix}".{"${src}","${trg}"}${ARTIFACT_EXT} \
   --valid-translation-output "${model_dir}/devset.out" \
   --quiet-translation \
   --overwrite \
