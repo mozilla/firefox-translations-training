f0f8537a3c72e04316b05b866973636b9d998857
Ben Hearsum <ben@mozilla.com>
2023-06-01 16:17:54 -0400

    Add toolchain build for browsermt-marian
    This is a simple fork of the upstream marian project, so we can use the same build script for it. It looks like this was forked from an older version that we're using for upstream marian though, and I had to apply a few fixes to have it build properly - mostly around using a newer CUDA library.

--- taskcluster/scripts/toolchain/build-marian.sh
+++ taskcluster/scripts/toolchain/build-marian.sh
@@ -2,8 +2,18 @@
 set -e
 set -x
 
+pushd `dirname $0` &>/dev/null
+MY_DIR=$(pwd)
+popd &>/dev/null
+
+patch=${1:-none}
+
 export MARIAN_DIR=$MOZ_FETCHES_DIR/marian-source
 export CUDA_DIR=$MOZ_FETCHES_DIR/cuda-toolkit
+
+if [ "$patch" != "none" ]; then
+  patch -d ${MARIAN_DIR} -p1 < ${MY_DIR}/${patch}
+fi
 
 # TODO: consider not calling out to this since it's such a simple script...
 bash $VCS_PATH/pipeline/setup/compile-marian.sh "${MARIAN_DIR}/build" "$(nproc)"
