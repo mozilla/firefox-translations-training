d2f6e05738dcfbfafcf979a2463607525e9e7b00
Ben Hearsum <ben@mozilla.com>
2023-06-05 20:00:31 -0400

    Add `from_datasets:mono` transforms sequence
    This new transforms sequence is very similar to the existing `per_dataset` one, except it's intended to be used with `mono-src` and/or `mono-trg` dataset types, and adds a new `locale` substitution based on which category is being used. (Note that we still want `src_locale` and `trg_locale` present for things like names though.)
    
    This patch also fixes an oversight in `get_dataset_category` (now `get_dataset_categories`) that had it assuming that each dataset was only in one category (wrong!).

--- taskcluster/translations_taskgraph/transforms/from_datasets.py
+++ taskcluster/translations_taskgraph/transforms/from_datasets.py
@@ -74,6 +74,9 @@ SCHEMA = Schema(
 per_dataset = TransformSequence()
 per_dataset.add_validate(SCHEMA)
 
+mono = TransformSequence()
+mono.add_validate(SCHEMA)
+
 locales_only = TransformSequence()
 locales_only.add_validate(SCHEMA)
 
@@ -89,19 +92,20 @@ def shorten_dataset_name(dataset):
     )
 
 
-def get_dataset_category(provider, dataset, dataset_categories):
+def get_dataset_categories(provider, dataset, dataset_categories):
+    categories = set()
     for category, datasets in dataset_categories.items():
         if f"{provider}_{dataset}" in datasets:
-            return category
+            categories.add(category)
 
-    return ""
+    return categories
 
 
 @per_dataset.add
 def jobs_from_datasets(config, jobs):
     for job in jobs:
         dataset_config = job.pop("dataset-config", {})
-        include_categories = dataset_config.get("include-categories", [])
+        include_categories = set(dataset_config.get("include-categories", []))
         include_datasets = dataset_config.get("include-datasets", {})
         exclude_datasets = dataset_config.get("exclude-datasets", {})
         exclude_locales = dataset_config.get("exclude-locales", [])
@@ -121,8 +125,8 @@ def jobs_from_datasets(config, jobs):
                 if dataset in exclude_datasets.get(provider, {}):
                     continue
 
-                category = get_dataset_category(provider, dataset, training_datasets)
-                if include_categories and category not in include_categories:
+                categories = get_dataset_categories(provider, dataset, training_datasets)
+                if include_categories and not include_categories.intersection(categories):
                     continue
 
                 for pair in locale_pairs:
@@ -164,11 +168,89 @@ def jobs_from_datasets(config, jobs):
                     yield subjob
 
 
+@mono.add
+def jobs_for_mono_datasets(config, jobs):
+    for job in jobs:
+        dataset_config = job.pop("dataset-config", {})
+        include_categories = set(dataset_config.get("include-categories", []))
+        include_datasets = dataset_config.get("include-datasets", {})
+        exclude_datasets = dataset_config.get("exclude-datasets", {})
+        exclude_locales = dataset_config.get("exclude-locales", [])
+        substitution_fields = dataset_config.get("substitution-fields", [])
+        training_datasets = config.params.get("training_config", {}).get("datasets", {})
+
+        if "mono-src" in include_categories and "mono-trg" in include_categories:
+            raise Exception("from_datasets:mono can only use one of: `mono-src`, `mono-trg` in include_categories")
+
+        providers = include_datasets.keys()
+        if not providers:
+            providers = config.graph_config["datasets"].keys()
+
+        for provider in providers:
+            datasets = include_datasets.get(provider, {})
+            if not datasets:
+                datasets = config.graph_config["datasets"][provider]
+
+            for dataset, locale_pairs in datasets.items():
+                if dataset in exclude_datasets.get(provider, {}):
+                    continue
+
+                categories = get_dataset_categories(provider, dataset, training_datasets)
+                if include_categories and not include_categories.intersection(categories):
+                    continue
+
+                for pair in locale_pairs:
+                    if pair in exclude_datasets.get(provider, {}).get(dataset, []):
+                        continue
+
+                    if pair in exclude_locales:
+                        continue
+
+                    if "mono-src" in include_categories:
+                        locale = pair["src"]
+                    elif "mono-trg" in include_categories:
+                        locale = pair["trg"]
+                    else:
+                        raise Exception("Don't use `from_datasets:mono` without the `mono-src` or `mono-trg` category!")
+                    subjob = copy.deepcopy(job)
+                    subs = {
+                        "provider": provider,
+                        "dataset": dataset,
+                        "dataset_short": shorten_dataset_name(dataset),
+                        "dataset_sanitized": dataset.replace("/", "_").replace(".", "_"),
+                        "locale": locale,
+                        "src_locale": pair["src"],
+                        "trg_locale": pair["trg"],
+                    }
+                    for field in substitution_fields:
+                        container, subfield = subjob, field
+                        while "." in subfield:
+                            f, subfield = subfield.split(".", 1)
+                            container = container[f]
+
+                        container[subfield] = substitute(container[subfield], **subs)
+
+                    # If the job has command-context, add these values there
+                    # as well. These helps to avoid needing two levels of
+                    # substitution in a command.
+                    if subjob.get("run", {}).get("command-context") is not None:
+                        subjob["run"]["command-context"].update(subs)
+
+                    subjob.setdefault("attributes", {})
+                    subjob["attributes"]["provider"] = provider
+                    subjob["attributes"]["dataset"] = dataset
+                    subjob["attributes"]["locale"] = locale
+                    subjob["attributes"]["src_locale"] = pair["src"]
+                    subjob["attributes"]["trg_locale"] = pair["trg"]
+
+                    yield subjob
+
+
 @locales_only.add
 def jobs_from_locales(config, jobs):
     for job in jobs:
         dataset_config = job.pop("dataset-config", {})
-        include_categories = dataset_config.get("include-categories", [])
+        include_categories = set(dataset_config.get("include-categories", []))
         include_datasets = dataset_config.get("include-datasets", {})
         exclude_datasets = dataset_config.get("exclude-datasets", {})
         exclude_locales = dataset_config.get("exclude-locales", [])
@@ -190,8 +272,8 @@ def jobs_from_locales(config, jobs):
                 if dataset in exclude_datasets.get(provider, {}):
                     continue
 
-                category = get_dataset_category(provider, dataset, training_datasets)
-                if include_categories and category not in include_categories:
+                categories = get_dataset_categories(provider, dataset, training_datasets)
+                if include_categories and not include_categories.intersection(categories):
                     continue
 
                 for pair in locale_pairs:
