a84aa597b8e6f138e99254be931a34aa70dd31c5
Gabriel Bustamante <gbustamante@mozilla.com>
2023-06-07 14:33:18 -0500

    Simplify merge-mono.sh

--- pipeline/clean/merge-mono.sh
+++ pipeline/clean/merge-mono.sh
@@ -13,24 +13,15 @@ output=$1
 max_sent=$2
 datasets=( "${@:3}" )
 
-LOCALE="${LOCALE:-}"
 COMPRESSION_CMD="${COMPRESSION_CMD:-pigz}"
-ARTIFACT_EXT="${ARTIFACT_EXT:-gz}"
 
-if [ -z "${LOCALE}" ]; then
-  dir=$(dirname "${output}")
-  mkdir -p "${dir}"
-  ${COMPRESSION_CMD} -dc "${datasets[@]}" |
-    ${BIN}/dedupe |
-    shuf -n "${max_sent}" |
-    ${COMPRESSION_CMD} >"${output}"
-else
-  datasets=($( printf '%s\n' "${datasets[@]}"| grep ${LOCALE} ))
-  output="${output}/mono.${LOCALE}.${ARTIFACT_EXT}"
-  ${COMPRESSION_CMD} -dc "${datasets[@]}" |
-    ${BIN}/dedupe |
-    shuf -n "${max_sent}" |
-    ${COMPRESSION_CMD} >"${output}"
-fi
+dir=$(dirname "${output}")
+mkdir -p "${dir}"
+
+${COMPRESSION_CMD} -dc "${datasets[@]}" |
+  ${BIN}/dedupe |
+  shuf -n "${max_sent}" |
+  ${COMPRESSION_CMD} >"${output}"
+
 
 echo "###### Done: Merging monolingual datasets"
