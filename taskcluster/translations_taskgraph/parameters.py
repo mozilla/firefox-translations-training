9146c6be69956ccb681733c72d9e6a5ad61825d9
Ben Hearsum <ben@mozilla.com>
2023-06-07 12:20:05 -0400

    Implement `evaluate-finetuned-teacher` step

--- taskcluster/translations_taskgraph/parameters.py
+++ taskcluster/translations_taskgraph/parameters.py
@@ -13,7 +13,7 @@ from voluptuous import Optional, Required
 def get_defaults(_):
     return {
         "training_config": {
-            "target-stage": "merge-augmented",
+            "target-stage": "evaluate-finetuned-teacher",
             "experiment": {
                 "name": "training pipeline test config",
                 "src": "ru",
