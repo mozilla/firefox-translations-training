a78398811580f0c47a5891cc65fe60aa7d37a97e
Ben Hearsum <ben@mozilla.com>
2023-06-14 10:13:44 -0400

    Don't fail substitution if format values are missing
    This allows us to easily format a string multiple times in different places.

--- taskcluster/translations_taskgraph/util/substitution.py
+++ taskcluster/translations_taskgraph/util/substitution.py
@@ -1,3 +1,12 @@
+class PartialSubstitutionDict(dict):
+    """A dictionary that will return any missing keys as their formatable 
+       version. Useful when a string needs to be formatted multiple times
+       in different places to get to its final form."""
+
+    def __missing__(self, key):
+        return "{" + key + "}"
+
+
 def substitute(item, **subs):
     if isinstance(item, list):
         for i in range(len(item)):
@@ -5,11 +14,11 @@ def substitute(item, **subs):
     elif isinstance(item, dict):
         new_dict = {}
         for k, v in item.items():
-            k = k.format(**subs)
+            k = k.format_map(PartialSubstitutionDict(subs))
             new_dict[k] = substitute(v, **subs)
         item = new_dict
     elif isinstance(item, str):
-        item = item.format(**subs)
+        item = item.format_map(PartialSubstitutionDict(subs))
     else:
         item = item
 
