53b85f21c6ff12197453573015e7808a28c81d70
Ben Hearsum <ben@mozilla.com>
2023-06-19 12:49:10 -0400

    Support `translate-corpus` in our pipeline wrapper script.
    Unlike `split-{mono,corpus}` the differences here are nearly trivial -- it's unlikely this script will ever be changed in a way that only affects one of these kinds.

--- taskcluster/scripts/pipeline/translate-taskcluster.sh
+++ taskcluster/scripts/pipeline/translate-taskcluster.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 
 input=$1
 output_dir=$2
-other_args=( "${@:3}" )
+type=$3
+other_args=( "${@:4}" )
 
 pushd `dirname $0`/../../.. &>/dev/null
 VCS_ROOT=$(pwd)
@@ -15,15 +16,25 @@ mkdir -p "${output_dir}"
 
 zstd -d --rm "${input}"
 input="${input%.zst}"
+
+outfile="${input}.out"
+if [ "${type}" = "nbest" ]; then
+  outfile="${input}.nbest"
+fi
+
 # In Taskcluster, we always parallelize this step N ways. In rare cases, there
 # may not be enough input files to feed all of these jobs. If we received an
 # empty input file we have nothing to do other than copying the empty file
 # to the output file, simulating successfully completion.
 if [ -s "${input}" ]; then
-  ${VCS_ROOT}/pipeline/translate/translate.sh "${input}" "${other_args[@]}"
+  if [ "${type}" = "plain" ]; then
+    ${VCS_ROOT}/pipeline/translate/translate.sh "${input}" "${other_args[@]}"
+  elif [ "${type}" = "nbest" ]; then
+    ${VCS_ROOT}/pipeline/translate/translate-nbest.sh "${input}" "${other_args[@]}"
+  fi
 else
-  cp "${input}" "${input}".out
+  cp "${input}" "${outfile}"
 fi
 
-zstd --rm "${input}.out"
-cp "${input}.out.zst" "${output_dir}"
+zstd --rm "${outfile}"
+cp "${outfile}.zst" "${output_dir}"
