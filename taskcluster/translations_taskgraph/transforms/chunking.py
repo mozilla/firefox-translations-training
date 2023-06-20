7d88b963f912e44a17055d7637eb407b3f8b03f8
Ben Hearsum (he/him) <ben@mozilla.com>
2023-06-20 14:21:12 -0400

    Implement the remaining `mono-trg` parts of the pipeline in Taskcluster (#135)
    * Add config.yml entry for workers with 4 GPUs
    
    * Don't fail substitution if format values are missing
    
    This allows us to easily format a string multiple times in different places.
    
    * Add wrapper script for `translate` step.
    
    Similar to the `split` steps, we need this wrapper to handle a case where a chunk has nothing to process.
    
    * Add `translate-mono-trg` pipeline step
    
    The most notable part of this is that it's our first step that is parallelized many ways, and I've added a new transform to handle that.
    
    Also of note is that while it seems like we should be adding `translate-mono-src` at the same time as this...we cannot. That step is actually downstream of this (despite earlier steps like `merge-mono` being a common ancestor of both).
    
    * Add zstd support to translate/{collect,merge-corpus}.sh
    
    * Implement `collect-mono-trg` pipeline step
    
    This step again breaks a bit of new ground: being the first step that requires unchunking (that is: pulling artifacts from many upstreams, and using all of them to generate its own artifact). This is handled by augmenting the `chunking` transform to allow the addition of N blocks to a task definition, with any necessary substitutions done in those blocks.
    
    * Implement `merge-augmented` pipeline step.
    
    This step is the culmination of the `mono-trg` side of the DAG, and pulls in artifacts from a number of places to produce two artifacts that are used to power the teacher training part of the pipeline. There's no chunking, conditional dependencies, or even configuration from the training config to worry about - it's a very straightforward configuration.

---
+++ taskcluster/translations_taskgraph/transforms/chunking.py
@@ -0,0 +1,88 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import copy
+
+from taskgraph.transforms.base import TransformSequence
+from taskgraph.util.schema import Schema, optionally_keyed_by, resolve_keyed_by
+from voluptuous import ALLOW_EXTRA, Required, Optional
+
+from translations_taskgraph.util.dict_helpers import deep_get
+from translations_taskgraph.util.substitution import substitute
+
+CHUNK_SCHEMA = Schema(
+    {
+        Required("chunk-config"): {
+            Required("total-chunks"): {
+                Required("from-parameters"): str,
+            },
+            Optional("substitution-fields"): [str],
+        }
+    },
+    extra=ALLOW_EXTRA,
+)
+
+chunk = TransformSequence()
+chunk.add_validate(CHUNK_SCHEMA)
+
+
+UNCHUNK_SCHEMA = Schema(
+    {
+        Required("chunk-config"): {
+            Required("total-chunks"): {
+                Required("from-parameters"): str,
+            },
+            Optional("per-upstream-fields"): object,
+        }
+    },
+    extra=ALLOW_EXTRA,
+)
+
+unchunk = TransformSequence()
+unchunk.add_validate(UNCHUNK_SCHEMA)
+
+
+@chunk.add
+def chunk_jobs(config, jobs):
+    for job in jobs:
+        chunk_config = job.pop("chunk-config", None)
+        total_chunks = deep_get(config.params, chunk_config["total-chunks"]["from-parameters"])
+        
+        for this_chunk in range(1, total_chunks + 1):
+            subjob = copy.deepcopy(job)
+            
+            subs = {
+                "this_chunk": this_chunk,
+                "total_chunks": total_chunks,
+            }
+
+            for field in chunk_config["substitution-fields"]:
+                container, subfield = subjob, field
+                while "." in subfield:
+                    f, subfield = subfield.split(".", 1)
+                    container = container[f]
+
+                container[subfield] = substitute(container[subfield], **subs)
+
+            yield subjob
+
+
+@unchunk.add
+def do_unchunking(config, jobs):
+    for job in jobs:
+        chunk_config = job.pop("chunk-config", None)
+        total_chunks = deep_get(config.params, chunk_config["total-chunks"]["from-parameters"])
+        
+        for this_chunk in range(1, total_chunks + 1):
+            subs = {
+                "this_chunk": this_chunk,
+                "total_chunks": total_chunks,
+            }
+
+            for field, value in chunk_config.get("per-upstream-fields", {}).items():
+                if field not in job:
+                    field[job] = {}
+
+                job[field].update(substitute(copy.deepcopy(value), **subs))
+
+        yield job
