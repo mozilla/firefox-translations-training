f6d390a5befbfc92f59c13e27d604cee72383e35
Ben Hearsum <ben@mozilla.com>
2023-06-20 16:11:19 -0400

    Implement `merge-translated` pipeline step

--- taskcluster/translations_taskgraph/parameters.py
+++ taskcluster/translations_taskgraph/parameters.py
@@ -13,7 +13,7 @@ from voluptuous import Optional, Required
 def get_defaults(_):
     return {
         "training_config": {
-            "target-stage": "collect-mono-src",
+            "target-stage": "merge-translated",
             "experiment": {
                 "name": "training pipeline test config",
                 "src": "ru",
