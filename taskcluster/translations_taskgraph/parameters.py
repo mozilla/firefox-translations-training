00dbb9ffe647a82b8077f32588f1e2bbc4e4af10
Ben Hearsum <ben@mozilla.com>
2023-06-20 15:44:18 -0400

    Implement `translate-mono-src` pipeline step

--- taskcluster/translations_taskgraph/parameters.py
+++ taskcluster/translations_taskgraph/parameters.py
@@ -13,7 +13,7 @@ from voluptuous import Optional, Required
 def get_defaults(_):
     return {
         "training_config": {
-            "target-stage": "collect-corpus",
+            "target-stage": "translate-mono-src",
             "experiment": {
                 "name": "training pipeline test config",
                 "src": "ru",
