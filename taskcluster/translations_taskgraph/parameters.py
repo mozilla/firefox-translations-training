49f56e39a3f3188f945a01073503fa2032f9e175
Ben Hearsum <ben@mozilla.com>
2023-06-21 10:25:28 -0400

    Implement `alignments` pipeline step

--- taskcluster/translations_taskgraph/parameters.py
+++ taskcluster/translations_taskgraph/parameters.py
@@ -13,7 +13,7 @@ from voluptuous import Optional, Required
 def get_defaults(_):
     return {
         "training_config": {
-            "target-stage": "cefilter",
+            "target-stage": "alignments",
             "experiment": {
                 "name": "training pipeline test config",
                 "src": "ru",
