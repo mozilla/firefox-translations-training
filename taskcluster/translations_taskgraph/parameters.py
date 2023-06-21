04555ccd519178be7d5bc49697c2443d3ae4917a
Ben Hearsum <ben@mozilla.com>
2023-06-21 14:17:42 -0400

    Implement `evaluate-quantized` pipeline step

--- taskcluster/translations_taskgraph/parameters.py
+++ taskcluster/translations_taskgraph/parameters.py
@@ -13,7 +13,7 @@ from voluptuous import Optional, Required
 def get_defaults(_):
     return {
         "training_config": {
-            "target-stage": "quantize",
+            "target-stage": "evaluate-quantized",
             "experiment": {
                 "name": "training pipeline test config",
                 "src": "ru",
