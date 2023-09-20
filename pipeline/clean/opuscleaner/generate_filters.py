831ed747c8027af786a207165966629555554d6e
evgeny pavlov <epavlov@mozilla.com>
2023-09-20 12:09:19 -0700

    Run linter

--- pipeline/clean/opuscleaner/generate_filters.py
+++ pipeline/clean/opuscleaner/generate_filters.py
@@ -9,51 +9,54 @@ import os
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_prefix', metavar='INPUT_PREFIX', type=str, help='Dataset file prefix')
-    parser.add_argument('src', metavar='SRC', type=str, help='Source language code')
-    parser.add_argument('trg', metavar='TRG', type=str, help='Target language code')
-    parser.add_argument('dataset', metavar='DATASET', type=str, help='Dataset name')
-    parser.add_argument('output', metavar='OUTPUT_PATH', type=str, help='Write filter config here')
+    parser.add_argument(
+        "input_prefix", metavar="INPUT_PREFIX", type=str, help="Dataset file prefix"
+    )
+    parser.add_argument("src", metavar="SRC", type=str, help="Source language code")
+    parser.add_argument("trg", metavar="TRG", type=str, help="Target language code")
+    parser.add_argument("dataset", metavar="DATASET", type=str, help="Dataset name")
+    parser.add_argument("output", metavar="OUTPUT_PATH", type=str, help="Write filter config here")
 
     args = parser.parse_args()
-    input_prefix = args.input_prefix
     src = args.src
     trg = args.trg
     dataset = args.dataset
     output = args.output
 
     # look whether there are filters produced by OpusCleaner UI first
-    custom_filter = f'configs/{src}-{trg}/{dataset}.{src}-{trg}.filters.json'
+    custom_filter = f"configs/{src}-{trg}/{dataset}.{src}-{trg}.filters.json"
     # workaround: we use "_" to separate the dataset version for OPUS datasets and OpusCleaner uses "-"
     custom_filter_opus = None
-    idx = dataset.rfind('_')
+    idx = dataset.rfind("_")
     if idx:
-        dataset_opus = f'{dataset[:idx]}-{dataset[idx+1:]}'
-        custom_filter_opus = f'configs/{src}-{trg}/{dataset_opus}.{src}-{trg}.filters.json'
+        dataset_opus = f"{dataset[:idx]}-{dataset[idx+1:]}"
+        custom_filter_opus = f"configs/{src}-{trg}/{dataset_opus}.{src}-{trg}.filters.json"
 
     if custom_filter_opus and os.path.exists(custom_filter_opus):
         with open(custom_filter_opus) as f:
             config = json.load(f)
-            print(f'Using filter {custom_filter_opus}')
+            print(f"Using filter {custom_filter_opus}")
     elif os.path.exists(custom_filter):
         with open(custom_filter) as f:
             config = json.load(f)
-            print(f'Using filter {custom_filter}')
+            print(f"Using filter {custom_filter}")
     else:
         # if custom filter is not found, use defaults
         # note: do not call the folder with default filters "filters" because it's a magic work for opuscleaner-clean
         # and it starts processing such folder
-        with open('configs/default.filters.json') as f:
+        with open("configs/default.filters.json") as f:
             config_str = f.read()
-            config_str = config_str.replace('<src>', src).replace('<trg>', trg)
-            abs_path_patterns = os.path.abspath('configs/remove_frequent_patterns.txt')
-            config_str = config_str.replace('configs/remove_frequent_patterns.txt', abs_path_patterns)
+            config_str = config_str.replace("<src>", src).replace("<trg>", trg)
+            abs_path_patterns = os.path.abspath("configs/remove_frequent_patterns.txt")
+            config_str = config_str.replace(
+                "configs/remove_frequent_patterns.txt", abs_path_patterns
+            )
             config = json.loads(config_str)
-            print(f'Using filter default.filters.json')
+            print("Using filter default.filters.json")
 
-    with open(output, 'w') as f:
+    with open(output, "w") as f:
         json.dump(config, f, indent=2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
