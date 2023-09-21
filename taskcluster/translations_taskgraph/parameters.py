4f85c3ee6f1179200be2ad930d53c3cccbb3f703
evgeny pavlov <epavlov@mozilla.com>
2023-09-21 15:38:27 -0700

    Switch back to all

--- taskcluster/translations_taskgraph/parameters.py
+++ taskcluster/translations_taskgraph/parameters.py
@@ -14,7 +14,7 @@ from voluptuous import Optional, Required
 def get_defaults(_):
     return {
         "training_config": {
-            "target-stage": "merge_mono",
+            "target-stage": "all",
             "experiment": {
                 "name": "ci",
                 "src": "ru",
