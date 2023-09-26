b24d255153abc2c63f20aa53425d23d61247062f
evgeny pavlov <epavlov@mozilla.com>
2023-09-26 14:00:34 -0700

    Fix usage of custom config

--- pipeline/clean/opuscleaner/generate_filters.py
+++ pipeline/clean/opuscleaner/generate_filters.py
@@ -5,10 +5,10 @@ Generates filter config for a dataset based on defaults to use in OpusCleaner
 import argparse
 import json
 import os
-from typing import Dict, Optional
+from typing import Optional
 
 
-def find_custom_filter(src: str, trg: str, dataset: str) -> Optional[Dict]:
+def find_custom_filter(src: str, trg: str, dataset: str) -> Optional[str]:
     # TODO: we'll likely need to move to a separate repo for those
     # TODO: to not include all filters for all languages in TC artifacts
 
@@ -16,30 +16,25 @@ def find_custom_filter(src: str, trg: str, dataset: str) -> Optional[Dict]:
     idx = dataset.rfind("/") if "/" in dataset else dataset.rfind("_")
     dataset_opus = f"{dataset[:idx]}-{dataset[idx + 1:]}" if idx else ""
 
+    # note: do not call the folder with default filters "filters" because it's a magic word for opuscleaner-clean
+    # and it starts processing such folder
     paths = [
         f"configs/{src}-{trg}/{dataset}.{src}-{trg}.filters.json",
-        f"configs/{trg}-{src}/{dataset}.{trg}-{src}.filters.json",
-        f"configs/{trg}-{src}/{dataset_opus}.{trg}-{src}.filters.json",
         f"configs/{src}-{trg}/{dataset_opus}.{src}-{trg}.filters.json",
     ]
 
     for path in paths:
         if os.path.exists(path):
-            with open(path) as f:
-                config = json.load(f)
-                print(f"Found custom filter {path}")
-                return config
-
+            return path
     return None
 
 
-def build_default_config(src: str, trg: str) -> str:
-    # note: do not call the folder with default filters "filters" because it's a magic word for opuscleaner-clean
-    # and it starts processing such folder
+def build_config(config_path: str, src: str, trg: str) -> str:
     # TODO: ideally "other" for "deescape-special-chars" should be replaced to <trg> for supported languages
-    with open("configs/default.filters.json") as f:
+    with open(config_path) as f:
         config_str = f.read()
         config_str = config_str.replace("<src>", src).replace("<trg>", trg)
+        # this replacement is required for the custom filters that were copied from OpusCleaner UI too
         abs_path_patterns = os.path.abspath("configs/remove_frequent_patterns.txt")
         config_str = config_str.replace("configs/remove_frequent_patterns.txt", abs_path_patterns)
         return json.loads(config_str)
@@ -62,13 +57,10 @@ def main() -> None:
     output = args.output
 
     # look whether there are custom filters produced by OpusCleaner UI first
-    config = find_custom_filter(src, trg, dataset)
-    if config:
-        print("Using custom filter")
-    else:
-        # if a custom filter is not found, use defaults
-        config = build_default_config(src, trg)
-        print("Using filter default.filters.json")
+    # if a custom filter is not found, use defaults
+    filter_path = find_custom_filter(src, trg, dataset) or "configs/default.filters.json"
+    print(f"Using filter {filter_path}")
+    config = build_config(filter_path, src, trg)
 
     with open(output, "w") as f:
         json.dump(config, f, indent=2)
