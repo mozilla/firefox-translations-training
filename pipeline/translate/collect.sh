87939bb70f1013ed51bc6b97cd9faceea6894ae2
Gabriel Bustamante <gbustamante@mozilla.com>
2023-10-12 11:43:50 -0500

    Add a comment for future readers to collect.sh

--- pipeline/translate/collect.sh
+++ pipeline/translate/collect.sh
@@ -15,6 +15,10 @@ mono_path=$3
 COMPRESSION_CMD="${COMPRESSION_CMD:-pigz}"
 
 echo "### Collecting translations"
+# To ensure that `file.10.out` comes after `file.9.out` and so on:
+# 1. `find "${dir}" -name '*.out'`: This will list all the `.out` files in the directory.
+# 2. `sort -t '.' -k2,2n`: This sorts the filenames numerically based on the number after the `.`. The `-t '.'` tells `sort` to use the period as a delimiter, and `-k2,2n` tells it to sort numerically using the second field.
+# 3. `xargs cat`: This takes the sorted list of filenames and uses `cat` to concatenate them.
 find "${dir}" -name '*.out' | sort -t '.' -k2,2n | xargs cat | ${COMPRESSION_CMD} >"${output_path}"
 
 echo "### Comparing number of sentences in source and artificial target files"
