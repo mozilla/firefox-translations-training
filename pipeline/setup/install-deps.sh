cf51faa9bdb4ef8804a1c9e876825fceec2aff1a
Amit Moryossef <amitmoryossef@gmail.com>
2023-11-06 18:32:31 +0100

    Update intel keys from 2019 to 2023 (#243)
    I don't know why, but I was having trouble using the 2019 file, and it works for me with the 2023 file

--- pipeline/setup/install-deps.sh
+++ pipeline/setup/install-deps.sh
@@ -18,7 +18,7 @@ apt-get install -y build-essential libboost-system-dev libprotobuf10 \
   protobuf-compiler libprotobuf-dev openssl libssl-dev libgoogle-perftools-dev
 
 echo "### Installing Intel MKL"
-wget -qO- 'https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB' | apt-key add -
+wget -qO- 'https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB' | apt-key add -
 sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
 apt-get update
 apt-get install -y intel-mkl-64bit-2020.0-088
