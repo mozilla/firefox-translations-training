dcade162be7ed7bde13ca9daa9b08a0621931f27
Greg Tatum <tatum.creative@gmail.com>
2023-12-05 10:24:42 -0600

    Add documentation to merge-mono

--- pipeline/clean/merge-mono.sh
+++ pipeline/clean/merge-mono.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ##
-# Merges monolingual datasets
+# Merges monolingual datasets. The datasets are input as a glob-style string.
+# Each file in that directory will be used as input. The files will be deduplicated,
+# shuffled, and written out to a single archive.
+#
+# See: taskcluster/ci/merge-mono/kind.yml
 #
 
 set -x
@@ -9,18 +13,22 @@ test -v BIN
 
 echo "###### Merging monolingual datasets"
 
-output=$1
-max_sent=$2
-datasets=( "${@:3}" )
+#                      Example inputs:
+output=$1              # /builds/worker/artifacts/mono.en.zst
+max_sentences=$2       # 100000000
+datasets=( "${@:3}" )  # $MOZ_FETCHES_DIR/*.zst'
 
 COMPRESSION_CMD="${COMPRESSION_CMD:-pigz}"
+
+echo "### Merging the following datasets:"
+ls $datasets
 
 dir=$(dirname "${output}")
 mkdir -p "${dir}"
 
 ${COMPRESSION_CMD} -dc "${datasets[@]}" |
   ${BIN}/dedupe |
-  shuf -n "${max_sent}" |
+  shuf -n "${max_sentences}" |
   ${COMPRESSION_CMD} >"${output}"
 
 
