62f2526b27aa3ab7d98685f08af2aa2f4335b03e
Valentin Rigal <rigal@teklia.com>
2024-01-19 16:46:12 +0100

    Update tests

--- tests/test_tracking_cli.py
+++ tests/test_tracking_cli.py
@@ -103,8 +103,6 @@ def test_experiments(wandb_mock, getargs_mock, caplog, samples_dir, tmp_dir):
                 logging.INFO,
                 f"Parsing folder {samples_dir}/experiments/models/en-nl/prod/student",
             ),
-            (logging.INFO, "Reading metrics file mtdata_Neulab-tedtalks_test-1-eng-nld.metrics"),
-            (logging.INFO, "Reading metrics file flores_devtest.metrics"),
             (logging.INFO, "Reading logs stream."),
             (logging.INFO, "Successfully parsed 1002 lines"),
             (logging.INFO, "Found 550 training entries"),
@@ -114,8 +112,6 @@ def test_experiments(wandb_mock, getargs_mock, caplog, samples_dir, tmp_dir):
                 logging.INFO,
                 f"Parsing folder {samples_dir}/experiments/models/en-nl/prod/teacher-finetuned0",
             ),
-            (logging.INFO, "Reading metrics file mtdata_Neulab-tedtalks_test-1-eng-nld.metrics"),
-            (logging.INFO, "Reading metrics file flores_devtest.metrics"),
             (logging.INFO, "Reading logs stream."),
             (logging.INFO, "Successfully parsed 993 lines"),
             (logging.INFO, "Found 567 training entries"),
@@ -125,20 +121,17 @@ def test_experiments(wandb_mock, getargs_mock, caplog, samples_dir, tmp_dir):
                 logging.INFO,
                 f"Parsing folder {samples_dir}/experiments/models/en-nl/prod/teacher-finetuned1",
             ),
-            (logging.INFO, "Reading metrics file mtdata_Neulab-tedtalks_test-1-eng-nld.metrics"),
-            (logging.INFO, "Reading metrics file flores_devtest.metrics"),
             (logging.INFO, "Reading logs stream."),
             (logging.INFO, "Successfully parsed 1000 lines"),
             (logging.INFO, "Found 573 training entries"),
             (logging.INFO, "Found 191 validation entries"),
-            # Group extra logs
+            # Publish group files and quantized/evaluated metrics
             (
                 logging.INFO,
-                "Publishing 'en-nl' group 'prod' metrics and files to a last fake run 'group_logs'",
+                "Publishing 'en-nl/prod' evaluation metrics and files (fake run 'group_logs')",
             ),
-            # Metrics for `speed` directory
-            (logging.INFO, "Reading metrics file mtdata_Neulab-tedtalks_test-1-eng-nld.metrics"),
-            (logging.INFO, "Reading metrics file flores_devtest.metrics"),
+            (logging.INFO, "Found 2 quantized metrics"),
+            (logging.INFO, "Found 16 evaluation metrics"),
         ]
     )
     log_calls, metrics_calls = [], []
@@ -152,12 +145,12 @@ def test_experiments(wandb_mock, getargs_mock, caplog, samples_dir, tmp_dir):
     # Custom calls for .metrics files publication
     assert set([list(v.keys())[0] for c in metrics_calls for v in c.args]) == set(
         [
-            "Mtdata_neulab-tedtalks_test-1-eng-nld summary",
-            "Flores_devtest summary",
-            "Mtdata_neulab-tedtalks_test-1-eng-nld summary",
-            "Flores_devtest summary",
-            "Mtdata_neulab-tedtalks_test-1-eng-nld summary",
-            "Flores_devtest summary",
+            "mtdata_neulab-tedtalks_test-1-eng-nld",
+            "flores_devtest",
+            "mtdata_neulab-tedtalks_test-1-eng-nld",
+            "flores_devtest",
+            "mtdata_neulab-tedtalks_test-1-eng-nld",
+            "flores_devtest",
         ]
     )
 
