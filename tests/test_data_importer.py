284de8a8c7acaa1a58b5cc07cabc0727f4fd6027
Greg Tatum <tatum.creative@gmail.com>
2024-01-29 10:34:35 -0600

    Use run_task for dataset importing

--- tests/test_data_importer.py
+++ tests/test_data_importer.py
@@ -7,9 +7,10 @@ from fixtures import DataDir
 SRC = "ru"
 TRG = "en"
 ARTIFACT_EXT = "gz"
+COMPRESSION_CMD = "pigz"
 
 os.environ["ARTIFACT_EXT"] = ARTIFACT_EXT
-os.environ["COMPRESSION_CMD"] = "pigz"
+os.environ["COMPRESSION_CMD"] = COMPRESSION_CMD
 os.environ["SRC"] = SRC
 os.environ["TRG"] = TRG
 
@@ -47,20 +48,23 @@ def data_dir():
 
 
 @pytest.mark.parametrize(
-    "dataset",
+    "importer,dataset",
     [
-        "mtdata_Neulab-tedtalks_test-1-eng-rus",
-        "opus_ELRC-3075-wikipedia_health/v1",
-        "flores_dev",
-        "sacrebleu_wmt19",
+        ("mtdata", "Neulab-tedtalks_test-1-eng-rus"),
+        ("opus", "ELRC-3075-wikipedia_health_v1"),
+        ("flores", "dev"),
+        ("sacrebleu", "wmt19"),
     ],
 )
-def test_basic_corpus_import(dataset, data_dir):
-    prefix = data_dir.join(dataset)
-    output_src = f"{prefix}.{SRC}.{ARTIFACT_EXT}"
-    output_trg = f"{prefix}.{TRG}.{ARTIFACT_EXT}"
+def test_basic_corpus_import(importer, dataset, data_dir):
+    data_dir.run_task(
+        f"dataset-{importer}-{dataset}-en-ru",
+        env={"COMPRESSION_CMD": COMPRESSION_CMD, "ARTIFACT_EXT": ARTIFACT_EXT},
+    )
 
-    run_import("corpus", dataset, prefix)
+    prefix = data_dir.join(f"artifacts/{dataset}")
+    output_src = f"{prefix}.ru.gz"
+    output_trg = f"{prefix}.en.gz"
 
     assert os.path.exists(output_src)
     assert os.path.exists(output_trg)
@@ -68,15 +72,15 @@ def test_basic_corpus_import(dataset, data_dir):
     assert len(read_lines(output_trg)) > 0
 
 
-@pytest.mark.parametrize(
-    "params",
-    [
-        ("sacrebleu_aug-upper_wmt19", is_upper_case, AUG_MIN_RATE, AUG_MAX_RATE),
-        ("sacrebleu_aug-upper-strict_wmt19", is_upper_case, 1.0, 1.0),
-        ("sacrebleu_aug-title_wmt19", is_title_case, AUG_MIN_RATE, AUG_MAX_RATE),
-        ("sacrebleu_aug-title-strict_wmt19", is_title_case, 1.0, 1.0),
-    ],
-)
+augmentation_params = [
+    ("sacrebleu_aug-upper_wmt19", is_upper_case, AUG_MIN_RATE, AUG_MAX_RATE),
+    ("sacrebleu_aug-upper-strict_wmt19", is_upper_case, 1.0, 1.0),
+    ("sacrebleu_aug-title_wmt19", is_title_case, AUG_MIN_RATE, AUG_MAX_RATE),
+    ("sacrebleu_aug-title-strict_wmt19", is_title_case, 1.0, 1.0),
+]
+
+
+@pytest.mark.parametrize("params", augmentation_params, ids=[d[0] for d in augmentation_params])
 def test_specific_augmentation(params, data_dir):
     dataset, check_func, min_rate, max_rate = params
     prefix = data_dir.join(dataset)
