37568439a304b7f8e3d143061070097b0cddd92d
evgeny pavlov <epavlov@mozilla.com>
2024-03-21 16:55:02 -0700

    Add Marian server for testing

---
+++ utils/marian_client.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+A client that connects to a Marian server and translates text interactively.
+Run `python utils.marian_client.py` and type a text to translate in the terminal
+Source: https://github.com/marian-nmt/marian-dev/blob/master/scripts/server/client_example.py
+"""
+
+
+from __future__ import print_function, unicode_literals, division
+
+import sys
+import argparse
+
+from websocket import create_connection
+
+
+if __name__ == "__main__":
+    # handle command-line options
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--batch-size", type=int, default=1)
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
+
+    # open connection
+    ws = create_connection("ws://localhost:8886/translate".format(args.port))
+
+    count = 0
+    batch = ""
+    for line in sys.stdin:
+        count += 1
+        batch += line.decode('utf-8') if sys.version_info < (3, 0) else line
+        if count == args.batch_size:
+            # translate the batch
+            ws.send(batch)
+            result = ws.recv()
+            print(result.rstrip())
+
+            count = 0
+            batch = ""
+
+    if count:
+        # translate the remaining sentences
+        ws.send(batch)
+        result = ws.recv()
+        print(result.rstrip())
+
+    # close connection
+    ws.close()
