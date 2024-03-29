fab87a7a70413295e9c3992589e492332faf0f52
Evgeny Pavlov <epavlov@mozilla.com>
2024-03-28 18:10:02 -0700

    Add support of inline noise data augmentation (#502)
    * Add eflomal based aligner
    
    * Use new aligner for shortlist
    
    * Remove old aligner
    
    * Add Taskcluster steps for whitespace tokenized alignments
    
    * Move file to a renamed directory
    
    * Use Tags modifier in training
    
    * Update tests for alignments and shortlist
    
    * Add support of inline noise augmentation in data importer
    
    * Do not use slow inline noise augmentation in devset on CI
    
    * Remove the old alignments task
    
    * Add a test for student alignments
    
    * Fix alignments in training tests
    
    * Return matplotlib module after merge
    
    * Rename functions
    
    * Add more comments in the code
    
    * Remove compression env
    
    * Relock poetry

--- pipeline/train/train.sh
+++ pipeline/train/train.sh
@@ -20,6 +20,7 @@ model_dir=$7
 vocab=$8
 best_model_metric=$9
 # comma separated alignment paths that correspond to each training dataset
+# (required for Tags modifier and guided alignments for student training)
 # or None to train without alignments
 alignments=${10}
 # random seed, UINT
@@ -57,15 +58,12 @@ for index in "${!datasets[@]}"; do
 
     if [ "${alignments}" != "None" ] ; then
       train_aln="${alns[index]}"
-      # alignments are currently used only for student that uses one stage of training
       echo "### Generating tsv dataset with alignments ${alignments}"
       paste <(${COMPRESSION_CMD} -dc "${train_set_prefix}.${src}.${ARTIFACT_EXT}") \
             <(${COMPRESSION_CMD} -dc "${train_set_prefix}.${trg}.${ARTIFACT_EXT}") \
             <(${COMPRESSION_CMD} -dc "${train_aln}") \
             >"${tsv_dataset}"
       rm "${train_aln}"
-      # when using tsv, marian requires --guided-alignments argument to be an index of the alignments in the tsv file
-      extra_params+=("--guided-alignment" "2")
     else
       echo "### Generating tsv dataset"
       # OpusTrainer supports only tsv and gzip
@@ -80,8 +78,13 @@ for index in "${!datasets[@]}"; do
     sed -i -e "s#<dataset${index}>#${tsv_dataset}#g" "${new_config}"
 done
 
-# Replace the path to vocab (required for alignments to work)
+# Replace the path to vocab
+# OpusTrainer uses space tokenized alignments for inline noise (Tags modifier)
+# then detokenizes them and coverts to SentencePiece tokenized ones using the vocab to feed to Marian
 sed -i -e "s#<vocab>#${vocab}#g" "${new_config}"
+# Replace source and target languages. This can be useful for custom detokenizer parameter in Tags
+sed -i -e "s#<src>#${src}#g" "${new_config}"
+sed -i -e "s#<trg>#${trg}#g" "${new_config}"
 # Replace the random seed for teachers
 sed -i -e "s#<seed>#${seed}#g" "${new_config}"
 
