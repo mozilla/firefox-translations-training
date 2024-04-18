47becec68f35b4a27d078b71aa9f345565fc321d
Ben Hearsum <ben@mozilla.com>
2024-04-18 15:30:10 -0400

    allow runtime selection of worker classes through training config
    This patch adds a new `worker-classes` section of the training config which allows for selecting the "class" of worker to use (currently GCP spot or GCP standard) by kind, with support for a default. This allows us quite flexible configuration, eg: using spot instances for translation and standard ones for training. We will also be able to add one or more classes for Snakepit machines when we bring those online. The default value is `{"default": "gcp-spot"}` - which means that we'll use all spot machines by default. (We can change the default in `config.prod.yml` if desired.)
    
    Most of this patch is quite a boring addition of the new `worker_selection` transform to the pipeline kinds. The most notable part otherwise is a fairly big rework of most things to do with workers in `config.yml`:
    * The taskgraph-required `workers.aliases` is now a very simple, straightforward list of all available worker types.
    * A new `local-worker-aliases` has been introduced. This maps the generic names like `b-largegpu` to concrete worker types `by-worker-class`. This removes the need for `-standard` variants for the generic names.
    * This necessitated a new transform function that looks up the concrete worker type in each kind before we hand off to the `task` transforms. (Previously, that transform simply looked up things like `b-largegpu` in the `workers.aliases` block. If we had the ability to feed it `worker-class` information we could have kept all our mappings there - but I couldn't come up with a reasonable way to do this upstream.)

--- taskcluster/translations_taskgraph/transforms/worker_selection.py
+++ taskcluster/translations_taskgraph/transforms/worker_selection.py
@@ -8,19 +8,40 @@
 # runtime, or adjust in kinds when changing worker types.
 
 from taskgraph.transforms.base import TransformSequence
-from taskgraph.util.schema import resolve_keyed_by
+from taskgraph.util.schema import evaluate_keyed_by
 
 transforms = TransformSequence()
 
 
 @transforms.add
-def evaluate_keyed_by(config, jobs):
+def set_worker_type(config, jobs):
+    """Determines the general type of worker each task wants, which sometimes
+    depends on `tasks-for`. Tasks typically will end up specifying one of the
+    worker `aliases` from config.yml after this is evaluated, eg: b-cpu,
+    b-largegpu-largedisk."""
+
+    training_config = config.params.get("training_config")
+    worker_classes = training_config["taskcluster"]["worker-classes"]
+    worker_class = worker_classes.get(config.kind, worker_classes["default"])
     for job in jobs:
-        resolve_keyed_by(
-            job,
-            "worker-type",
-            item_name=job["description"],
-            **{"tasks-for": config.params["tasks_for"]},
+        # First, evaluate the `keyed-by` in the initial task specification from
+        # the kind, if present. This should give us one of the keys from
+        # `worker-configuration` in config.yml.
+        task_worker_type = evaluate_keyed_by(
+            job["worker-type"],
+            job["description"],
+            {"tasks-for": config.params["tasks_for"]},
+        )
+
+        # Now that we have one of the aliases, we need to resolve it to a
+        # specific worker type, as some of those aliases have their own
+        # `keyed-by` blocks, which may give different worker types depending
+        # on what's in the training config.
+        worker_alias_block = config.graph_config["local-worker-aliases"][task_worker_type].copy()
+        job["worker-type"] = evaluate_keyed_by(
+            worker_alias_block,
+            task_worker_type,
+            {"worker-class": worker_class},
         )
 
         yield job
@@ -30,24 +51,20 @@ def evaluate_keyed_by(config, jobs):
 def inject_worker_env(config, jobs):
     for job in jobs:
         # This is called worker-type in jobs, but in reality it's an alias resolved in the graph config...
-        worker_alias = job["worker-type"]
+        worker_type = job["worker-type"]
+        worker_config = config.graph_config["worker-configuration"].get(worker_type, {})
 
-        worker_definition = config.graph_config["workers"]["aliases"].get(worker_alias)
-        if not worker_definition:
-            raise Exception(f"Couldn't find worker definition for {worker_alias} in graph config!")
-
-        worker_type = worker_definition["worker-type"]
-        worker_config = config.graph_config["worker-configuration"].get(worker_type)
-        if not worker_config:
-            raise Exception(
-                f"Couldn't find worker configuration for {worker_type} in graph config!"
-            )
-
-        worker_env = worker_config["env"]
+        worker_env = worker_config.get("env", {})
         if "GPUS" not in worker_env or "WORKSPACE" not in worker_env:
-            raise Exception(
-                "GPUS and/or WORKSPACE values missing from worker env, this is probably misconfiguration."
-            )
+            # GPU tasks will not function correctly without these set; make this an error
+            # before they even run.
+            if "gpu" in worker_type:
+                raise Exception(
+                    "GPUS and/or WORKSPACE values missing from worker env, this is probably misconfiguration."
+                )
+            else:
+                yield job
+                continue
 
         job["worker"]["env"].update(worker_env)
 
