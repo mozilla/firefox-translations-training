73eeb3c36ae8f2c0baa01f876eec91a22df3d56b
evgeny pavlov <epavlov@mozilla.com>
2024-04-24 10:54:09 -0700

    Saves scores in jsonl

--- pipeline/bicleaner/save_scores.py
+++ pipeline/bicleaner/save_scores.py
@@ -8,13 +8,13 @@ assert sys.argv[1].endswith('.zst')
 
 print('reading {}'.format(sys.argv[1]))
 
-with zstandard.open(sys.argv[1], 'rt') as f:
-    for line in f:
-        score = float(line.rstrip().split('\t')[-1])
-        scores.append({'BicleanerAI': [score]})
+with zstandard.open(sys.argv[1], 'rt') as fr:
+    with open(sys.argv[2], 'w') as fw:
+        for line in fr:
+            score = float(line.rstrip().split('\t')[-1])
+            fw.write(json.dumps({'BicleanerAI': [score]}))
+            fw.write('\n')
 
-with open(sys.argv[2], 'w') as f:
-    json.dump(scores, f)
 
 print('Saved to {}'.format(sys.argv[2]))
 
