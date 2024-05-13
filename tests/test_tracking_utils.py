1c4020b06208bb1e055d8d4e8a1f133580d5ee45
Valentin Rigal <rigal@teklia.com>
2024-05-13 14:36:38 +0200

    Update other tests

--- tests/test_tracking_utils.py
+++ tests/test_tracking_utils.py
@@ -8,15 +8,15 @@ from tracking.translations_parser.utils import build_task_name, parse_tag
     [
         (
             "evaluate-teacher-flores-flores_aug-title_devtest-lt-en-1_2",
-            ("teacher", "flores", "devtest", "aug-title"),
+            ("teacher-1", "flores", "devtest", "aug-title"),
         ),
         (
-            "evaluate-quantized-mtdata_aug-mix_Neulab-tedtalks_eng-lit-lt-en-1_2",
-            ("quantized", "mtdata", "Neulab-tedtalks_eng-lit", "aug-mix"),
+            "evaluate-quantized-mtdata_aug-mix_Neulab-tedtalks_eng-lit-lt-en-2_2",
+            ("quantized-2", "mtdata", "Neulab-tedtalks_eng-lit", "aug-mix"),
         ),
         (
             "evaluate-finetuned-student-sacrebleu-wmt19-lt-en",
-            ("finetuned-student", "sacrebleu", "wmt19", None),
+            ("finetuned-student-1", "sacrebleu", "wmt19", None),
         ),
         (
             "evaluate-student-2-sacrebleu-wmt19-lt-en",
@@ -24,27 +24,27 @@ from tracking.translations_parser.utils import build_task_name, parse_tag
         ),
         (
             "train-student-en-hu",
-            ("student", None, None, None),
+            ("student-1", None, None, None),
         ),
         (
             "eval_teacher-ensemble_mtdata_Neulab-tedtalks_test-1-eng-nld",
-            ("teacher-ensemble", "mtdata", "Neulab-tedtalks_test-1-eng-nld", None),
+            ("teacher-ensemble-1", "mtdata", "Neulab-tedtalks_test-1-eng-nld", None),
         ),
         (
             "eval_student-finetuned_flores_devtest",
-            ("student-finetuned", "flores", "devtest", None),
+            ("student-finetuned-1", "flores", "devtest", None),
         ),
         (
             "eval_teacher-base0_flores_devtest",
-            ("teacher-base0", "flores", "devtest", None),
+            ("teacher-base-0", "flores", "devtest", None),
         ),
         (
             "train-backwards-en-ca",
-            ("backwards", None, None, None),
+            ("backwards-1", None, None, None),
         ),
         (
             "evaluate-teacher-flores-flores_dev-en-ca-1/2",
-            ("teacher", "flores", "dev", None),
+            ("teacher-1", "flores", "dev", None),
         ),
     ],
 )
