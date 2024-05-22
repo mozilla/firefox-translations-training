5c009ae9982066debcb54ba1876c3efcc9ae1470
evgeny pavlov <epavlov@mozilla.com>
2024-05-22 16:40:58 -0700

    Revert "TRASHME test parameters to trigger train in CI"
    This reverts commit ede424578665c0947b99281382aad9729077aff3.

--- taskcluster/translations_taskgraph/parameters.py
+++ taskcluster/translations_taskgraph/parameters.py
@@ -105,7 +105,7 @@ def get_defaults(_):
                 },
             },
             # Disable Weight & Biases publication on CI
-            "wandb-publication": True,
+            "wandb-publication": False,
         },
     }
 
