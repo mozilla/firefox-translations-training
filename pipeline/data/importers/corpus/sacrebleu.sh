1f539569ba9b091cc9d1c5fe3e97b197722f9e0b
Greg Tatum <gregtatum@users.noreply.github.com>
2024-06-03 15:56:21 -0500

    Fix data importing for sacrebleu (#658)

--- pipeline/data/importers/corpus/sacrebleu.sh
+++ pipeline/data/importers/corpus/sacrebleu.sh
@@ -16,7 +16,41 @@ dataset=$4
 COMPRESSION_CMD="${COMPRESSION_CMD:-pigz}"
 ARTIFACT_EXT="${ARTIFACT_EXT:-gz}"
 
-sacrebleu -t "${dataset}" -l "${src}-${trg}" --echo src | ${COMPRESSION_CMD} -c > "${output_prefix}.${src}.${ARTIFACT_EXT}"
-sacrebleu -t "${dataset}" -l "${src}-${trg}" --echo ref | ${COMPRESSION_CMD} -c > "${output_prefix}.${trg}.${ARTIFACT_EXT}"
+{
+  set +e
+
+  sacrebleu                         \
+    --test-set "${dataset}"         \
+    --language-pair "${src}-${trg}" \
+    --echo src                      \
+  | ${COMPRESSION_CMD} -c > "${output_prefix}.${src}.${ARTIFACT_EXT}"
+
+  sacrebleu                         \
+    --test-set "${dataset}"         \
+    --language-pair "${src}-${trg}" \
+    --echo ref                      \
+  | ${COMPRESSION_CMD} -c > "${output_prefix}.${trg}.${ARTIFACT_EXT}"
+
+  status=$?
+
+  set -e
+}
+
+if [ $status -ne 0 ]; then
+  echo "The first import failed, try again by switching the language pair direction."
+
+  sacrebleu                         \
+    --test-set "${dataset}"         \
+    --language-pair "${trg}-${src}" \
+    --echo src                      \
+    | ${COMPRESSION_CMD} -c > "${output_prefix}.${trg}.${ARTIFACT_EXT}"
+
+  sacrebleu                         \
+    --test-set "${dataset}"         \
+    --language-pair "${trg}-${src}" \
+    --echo ref                      \
+    | ${COMPRESSION_CMD} -c > "${output_prefix}.${src}.${ARTIFACT_EXT}"
+fi
+
 
 echo "###### Done: Downloading sacrebleu corpus"
