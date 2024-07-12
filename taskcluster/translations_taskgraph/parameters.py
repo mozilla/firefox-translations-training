65877d883e594e2beddf72ac939dfe47d524d296
Valentin Rigal <rigal@teklia.com>
2024-07-12 09:34:10 +0200

    Revert "TRASHME Test publication from CI"
    This reverts commit 4e15ed4eb48967fc0ef301ad840c0d17dc91b804.

--- taskcluster/translations_taskgraph/parameters.py
+++ taskcluster/translations_taskgraph/parameters.py
@@ -38,14 +38,14 @@ def get_defaults(_):
             },
             "marian-args": {
                 "training-backward": {
-                    "disp-freq": "6",
+                    "disp-freq": "2",
                     "save-freq": "25",
                     "valid-freq": "50",
                     "after": "50u",
                     "dim-vocabs": "1000 1000",
                 },
                 "training-teacher": {
-                    "disp-freq": "6",
+                    "disp-freq": "1",
                     "save-freq": "25",
                     "valid-freq": "50",
                     "after": "50u",
@@ -53,14 +53,14 @@ def get_defaults(_):
                     "task": "transformer-base",
                 },
                 "training-student": {
-                    "disp-freq": "6",
+                    "disp-freq": "1",
                     "save-freq": "25",
                     "valid-freq": "50",
                     "after": "50u",
                     "dim-vocabs": "1000 1000",
                 },
                 "training-student-finetuned": {
-                    "disp-freq": "6",
+                    "disp-freq": "1",
                     "save-freq": "25",
                     "valid-freq": "50",
                     "after": "50u",
@@ -106,7 +106,7 @@ def get_defaults(_):
                 },
             },
             # Disable Weight & Biases publication on CI
-            "wandb-publication": True,
+            "wandb-publication": False,
         },
     }
 
