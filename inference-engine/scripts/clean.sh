cf23bf758582c4c8cd564fa5c21ac0772e95aec3
Erik Nordin <enordin@mozilla.com>
2024-09-26 13:07:27 -0500

    Add clean script to inference-engine

---
+++ inference-engine/scripts/clean.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# Run script from the context of inference-engine directory
+cd "$(dirname $0)/.."
+
+# Ensure script is running within docker
+./scripts/detect-docker.sh inference-engine-clean
+
+# List of directories to clean
+dirs=("build-local" "build-wasm" "emsdk")
+
+# Flag to track if any directories were cleaned
+cleaned=false
+
+# Check and remove directories
+for dir in "${dirs[@]}"; do
+    if [ -d "$dir" ]; then
+        echo "Removing $dir..."
+        rm -rf "$dir"
+        cleaned=true
+    fi
+done
+
+# If no directories were cleaned, print a message
+if [ "$cleaned" = false ]; then
+    echo "Nothing to clean"
+fi
+
