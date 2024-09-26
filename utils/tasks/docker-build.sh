f0946dfb7d73f1a5902149a25ace1e5d6e5b1762
Greg Tatum <tatum.creative@gmail.com>
2024-09-26 13:24:19 -0500

    Use build_image directives

--- utils/tasks/docker-build.sh
+++ utils/tasks/docker-build.sh
@@ -5,14 +5,8 @@ set -x
 
 source utils/tasks/docker-setup.sh
 
-docker build \
-  --file taskcluster/docker/base/Dockerfile \
-  --tag ftt-base .
-
-docker build \
-  --build-arg DOCKER_IMAGE_PARENT=ftt-base \
-  --file taskcluster/docker/test/Dockerfile \
-  --tag ftt-test .
+DOCKER_IMAGE_PARENT=ftt-base taskgraph build-image base --tag ftt-base
+taskgraph build-image test --tag ftt-test
 
 docker build \
   --build-arg DOCKER_IMAGE_PARENT=ftt-test \
