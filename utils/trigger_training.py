7d05504a0aea2d39ad06dbc4e89bc2b14585f30f
Greg Tatum <tatum.creative@gmail.com>
2024-10-17 13:49:21 -0500

    Kick off training from the command line

--- utils/trigger_training.py
+++ utils/trigger_training.py
@@ -1,5 +1,5 @@
 """
-Open up the train task from the CLI based on your current branch.
+Trigger a training task from the CLI on your current branch.
 
 For example:
 
@@ -11,20 +11,31 @@ from pathlib import Path
 import subprocess
 import sys
 from time import sleep
+from typing import Optional, Tuple
 from github import Github
-import taskcluster
-import webbrowser
-import pyperclip
+import os
+import yaml
+import jsone
+from taskgraph.util.taskcluster import get_artifact
+from taskcluster import Hooks
+from taskcluster.helper import TaskclusterConfig
+
+ROOT_URL = "https://firefox-ci-tc.services.mozilla.com"
 
 
-def run(command: list[str]):
-    return subprocess.run(command, capture_output=True, check=True, text=True).stdout.strip()
+def run(command: list[str], env={}):
+    return subprocess.run(
+        command, capture_output=True, check=True, text=True, env={**os.environ, **env}
+    ).stdout.strip()
 
 
 def check_if_pushed(branch: str) -> bool:
-    remote_commit = run(["git", "rev-parse", f"origin/{branch}"])
-    local_commit = run(["git", "rev-parse", branch])
-    return local_commit == remote_commit
+    try:
+        remote_commit = run(["git", "rev-parse", f"origin/{branch}"])
+        local_commit = run(["git", "rev-parse", branch])
+        return local_commit == remote_commit
+    except subprocess.CalledProcessError:
+        return False
 
 
 def get_decision_task_push(branch: str):
@@ -44,20 +55,110 @@ def get_decision_task_push(branch: str):
     return decision_task
 
 
-def get_task_group_id(task_url: str):
+def get_task_id_from_url(task_url: str):
     """
-    Look up the task group ID from a task url.
+    Extract the task id from a task url
 
     e.g. https://firefox-ci-tc.services.mozilla.com/tasks/PhAMJTZBSmSeWStXbR72xA
+         returns
+         "PhAMJTZBSmSeWStXbR72xA"
     """
-    task_id = task_url.split("/")[-1]
+    return task_url.split("/")[-1]
 
-    print("Decision Task ID:", task_id)
-    queue = taskcluster.Queue({"rootUrl": "https://firefox-ci-tc.services.mozilla.com"})
-    task = queue.task(task_id)
-    task_group_id = task["taskGroupId"]
-    print("Train Action Group ID:", task_group_id)
-    return task_group_id
+
+def get_train_action(decision_task_id: str):
+    actions_json = get_artifact(decision_task_id, "public/actions.json")
+
+    for action in actions_json["actions"]:
+        if action["name"] == "train":
+            return action
+
+    print("Could not find the train action.")
+    print(actions_json)
+    sys.exit(1)
+
+
+def trigger_training(decision_task_id: str, config: dict[str, any]):
+    taskcluster = TaskclusterConfig(ROOT_URL)
+    taskcluster.auth()
+    hooks: Hooks = taskcluster.get_service("hooks")
+    train_action = get_train_action(decision_task_id)
+
+    # Render the payload using the jsone schema.
+    hook_payload = jsone.render(
+        train_action["hookPayload"],
+        {
+            "input": config,
+            "taskId": None,
+            "taskGroupId": decision_task_id,
+        },
+    )
+
+    confirmation = input("\nStart training? [Y,n]\n")
+    if confirmation and confirmation.lower() != "y":
+        return
+
+    # https://docs.taskcluster.net/docs/reference/core/hooks/api#triggerHook
+    response = hooks.triggerHook(train_action["hookGroupId"], train_action["hookId"], hook_payload)
+
+    action_task_id = response["status"]["taskId"]
+
+    print(f"Train action triggered: {ROOT_URL}/tasks/{action_task_id}")
+
+
+def validate_taskcluster_credentials():
+    try:
+        run(["taskcluster", "--help"])
+    except Exception:
+        print("The taskcluster client library must be installed on the system.")
+        print("https://github.com/taskcluster/taskcluster/tree/main/clients/client-shell")
+        sys.exit(1)
+
+    if not os.environ.get("TASKCLUSTER_ACCESS_TOKEN"):
+        print("You must log in to Taskcluster. Run the following:")
+        print(f'eval `TASKCLUSTER_ROOT_URL="{ROOT_URL}" taskcluster signin`')
+        sys.exit(1)
+
+    try:
+        run(
+            [
+                "taskcluster",
+                "signin",
+                "--check",
+            ],
+            {"TASKCLUSTER_ROOT_URL": ROOT_URL},
+        )
+    except Exception:
+        print("Your Taskcluster credentials have expired. Run the following:")
+        print(f'eval `TASKCLUSTER_ROOT_URL="{ROOT_URL}" taskcluster signin`')
+        sys.exit(1)
+
+
+def log_config_info(config_path: Path, config: dict):
+    print(f"\nUsing config: {config_path}\n")
+
+    experiment = config["experiment"]
+    config_details: list[Tuple[str, any]] = []
+    config_details.append(("experiment.name", experiment["name"]))
+    config_details.append(("experiment.src", experiment["src"]))
+    config_details.append(("experiment.trg", experiment["trg"]))
+    config_details.append(("target-stage", config["target-stage"]))
+
+    previous_group_ids = config.get("previous_group_ids")
+    if previous_group_ids:
+        config_details.append(("previous_group_ids", previous_group_ids))
+
+    pretrained_models: Optional[dict] = experiment.get("pretrained-models")
+    if pretrained_models:
+        for key, value in pretrained_models.items():
+            config_details.append((key, value))
+
+    key_len = 0
+    for key, _ in config_details:
+        key_len = max(key_len, len(key))
+
+    for key, value in config_details:
+        print(f"{key.rjust(key_len + 4, " ")}: {value}")
 
 
 def main() -> None:
@@ -79,9 +180,16 @@ def main() -> None:
         action="store_true",
         help="Skip the checks for the branch being up to date",
     )
+    parser.add_argument(
+        "--no_interactive",
+        action="store_true",
+        help="Skip the confirmation",
+    )
 
     args = parser.parse_args()
     branch = args.branch
+
+    validate_taskcluster_credentials()
 
     if branch:
         print(f"Using --branch: {branch}")
@@ -129,18 +237,13 @@ def main() -> None:
 
         sleep(timeout)
 
-    task_group_id = get_task_group_id(decision_task.details_url)
+    decision_task_id = get_task_id_from_url(decision_task.details_url)
 
-    config: Path = args.config
+    with args.config.open() as file:
+        config: dict = yaml.safe_load(file)
 
-    url = f"https://firefox-ci-tc.services.mozilla.com/tasks/groups/{task_group_id}"
-    print(f"Opening {url}")
-
-    with config.open("rt", encoding="utf-8") as file:
-        print('The config has been copied to your clipboard. Select "Train" from the actions.')
-        pyperclip.copy(file.read())
-
-    webbrowser.open(url)
+    log_config_info(args.config, config)
+    trigger_training(decision_task_id, config)
 
 
 if __name__ == "__main__":
