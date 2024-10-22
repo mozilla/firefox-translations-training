84708b53997188d4a63492db787be53eedea76ea
Greg Tatum <tatum.creative@gmail.com>
2024-10-22 08:10:14 -0500

    Change opustrainer log level

--- pipeline/train/train.py
+++ pipeline/train/train.py
@@ -279,7 +279,7 @@ class TrainCLI:
                 {
                     "config": self.opustrainer_config,
                     "log-file": self.artifacts / "opustrainer.log",
-                    "log-level": "INFO",
+                    "log-level": "ERROR",
                 }
             ),
         ]
