5a0ccd94fde8343a974e768a4da800185b37a52c
Erik Nordin <enordin@mozilla.com>
2024-10-23 15:41:23 -0500

    Replace build-wasm.sh with build-wasm.py
    This patch modifies build-wasm.py from its original state that
    was copied over in the previous commit to be used directly within
    this project.
    
    It then removes build-wasm.sh and replaces all instances with the
    new Python build script.

--- inference/scripts/build-wasm.py
+++ inference/scripts/build-wasm.py
@@ -1,17 +1,4 @@
 #!/usr/bin/env python3
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-"""
-Builds the Bergamot translations engine for integration with Firefox.
-
-If you wish to test the Bergamot engine locally, then uncomment the .wasm line in
-the toolkit/components/translations/jar.mn after building the file. Just make sure
-not to check the code change in.
-"""
-
 import argparse
 import multiprocessing
 import os
@@ -19,30 +6,28 @@ import shutil
 import subprocess
 from collections import namedtuple
 
-import yaml
-
-DIR_PATH = os.path.realpath(os.path.dirname(__file__))
-THIRD_PARTY_PATH = os.path.join(DIR_PATH, "thirdparty")
-MOZ_YAML_PATH = os.path.join(DIR_PATH, "moz.yaml")
-PATCHES_PATH = os.path.join(DIR_PATH, "patches")
-BERGAMOT_PATH = os.path.join(THIRD_PARTY_PATH, "bergamot-translator")
-MARIAN_PATH = os.path.join(BERGAMOT_PATH, "3rd_party/marian-dev")
-GEMM_SCRIPT = os.path.join(BERGAMOT_PATH, "wasm/patch-artifacts-import-gemm-module.sh")
-BUILD_PATH = os.path.join(THIRD_PARTY_PATH, "build-wasm")
+SCRIPTS_PATH = os.path.realpath(os.path.dirname(__file__))
+INFERENCE_PATH = os.path.dirname(SCRIPTS_PATH)
+PROJECT_ROOT_PATH = os.path.dirname(INFERENCE_PATH)
+BUILD_PATH = os.path.join(INFERENCE_PATH, "build-wasm")
+THIRD_PARTY_PATH = os.path.join(INFERENCE_PATH, "3rd_party")
+MARIAN_PATH = os.path.join(THIRD_PARTY_PATH, "browsermt-marian-dev")
 EMSDK_PATH = os.path.join(THIRD_PARTY_PATH, "emsdk")
 EMSDK_ENV_PATH = os.path.join(EMSDK_PATH, "emsdk_env.sh")
 WASM_PATH = os.path.join(BUILD_PATH, "bergamot-translator-worker.wasm")
 JS_PATH = os.path.join(BUILD_PATH, "bergamot-translator-worker.js")
-FINAL_JS_PATH = os.path.join(DIR_PATH, "bergamot-translator.js")
-ROOT_PATH = os.path.join(DIR_PATH, "../../../..")
+PATCHES_PATH = os.path.join(INFERENCE_PATH, "patches")
+BUILD_DIRECTORY = os.path.join(INFERENCE_PATH, "build-wasm")
+GEMM_SCRIPT = os.path.join(INFERENCE_PATH, "wasm", "patch-artifacts-import-gemm-module.sh")
+DETECT_DOCKER_SCRIPT = os.path.join(SCRIPTS_PATH, "detect-docker.sh")
 
 # 3.1.47 had an error compiling sentencepiece.
 EMSDK_VERSION = "3.1.8"
 EMSDK_REVISION = "2346baa7bb44a4a0571cc75f1986ab9aaa35aa03"
 
 patches = [
-    (BERGAMOT_PATH, os.path.join(PATCHES_PATH, "allocation-bergamot.patch")),
-    (MARIAN_PATH, os.path.join(PATCHES_PATH, "allocation-marian.patch")),
+    (MARIAN_PATH, os.path.join(PATCHES_PATH, "01-marian-fstream-for-macos.patch")),
+    (MARIAN_PATH, os.path.join(PATCHES_PATH, "02-marian-allocation.patch")),
 ]
 
 parser = argparse.ArgumentParser(
@@ -58,6 +43,16 @@ parser.add_argument(
 )
 
 ArgNamespace = namedtuple("ArgNamespace", ["clobber", "debug"])
+
+
+def ensure_docker():
+    """Ensure the script is running inside Docker."""
+    subprocess.run(
+        [DETECT_DOCKER_SCRIPT, "inference-build-wasm"],
+        cwd=INFERENCE_PATH,
+        shell=True,
+        check=True,
+    )
 
 
 def git_clone_update(name: str, repo_path: str, repo_url: str, revision: str):
@@ -106,20 +101,6 @@ def install_and_activate_emscripten(args: ArgNamespace):
     run_shell("./emsdk activate " + EMSDK_VERSION)
 
 
-def install_bergamot():
-    with open(MOZ_YAML_PATH, "r", encoding="utf8") as file:
-        text = file.read()
-
-    moz_yaml = yaml.safe_load(text)
-
-    git_clone_update(
-        name="bergamot",
-        repo_path=BERGAMOT_PATH,
-        repo_url=moz_yaml["origin"]["url"],
-        revision=moz_yaml["origin"]["revision"],
-    )
-
-
 def to_human_readable(size):
     """Convert sizes to human-readable format"""
     size_in_mb = size / 1048576
@@ -128,12 +109,12 @@ def to_human_readable(size):
 
 def apply_git_patch(repo_path, patch_path):
     print(f"Applying patch {patch_path} to {os.path.basename(repo_path)}")
-    subprocess.check_call(["git", "apply", "--reject", patch_path], cwd=repo_path)
+    subprocess.check_call(["git", "apply", "--reject", patch_path], cwd=PROJECT_ROOT_PATH)
 
 
 def revert_git_patch(repo_path, patch_path):
     print(f"Reverting patch {patch_path} from {os.path.basename(repo_path)}")
-    subprocess.check_call(["git", "apply", "-R", "--reject", patch_path], cwd=repo_path)
+    subprocess.check_call(["git", "apply", "-R", "--reject", patch_path], cwd=PROJECT_ROOT_PATH)
 
 
 def build_bergamot(args: ArgNamespace):
@@ -143,7 +124,7 @@ def build_bergamot(args: ArgNamespace):
     if not os.path.exists(BUILD_PATH):
         os.mkdir(BUILD_PATH)
 
-    print("\n 🖌️ Applying source code patches\n")
+    print("\n🖌️ Applying source code patches\n")
     for repo_path, patch_path in patches:
         apply_git_patch(repo_path, patch_path)
 
@@ -165,13 +146,13 @@ def build_bergamot(args: ArgNamespace):
         if args.debug:
             flags = "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
 
-        print("\n 🏃 Running CMake for Bergamot\n")
-        run_shell(f"emcmake cmake -DCOMPILE_WASM=on -DWORMHOLE=off {flags} {BERGAMOT_PATH}")
+        print("\n🏃 Running CMake for Bergamot\n")
+        run_shell(f"emcmake cmake -DCOMPILE_WASM=on -DWORMHOLE=off {flags} {INFERENCE_PATH}")
 
-        print("\n 🏃 Building Bergamot with emmake\n")
+        print("\n🏃 Building Bergamot with emmake\n")
         run_shell(f"emmake make -j {multiprocessing.cpu_count()}")
 
-        print("\n 🪚 Patching Bergamot for gemm support\n")
+        print("\n🪚 Patching Bergamot for gemm support\n")
         subprocess.check_call(["bash", GEMM_SCRIPT, BUILD_PATH])
 
         print("\n✅ Build complete\n")
@@ -196,71 +177,17 @@ def build_bergamot(args: ArgNamespace):
             revert_git_patch(repo_path, patch_path)
 
 
-def write_final_bergamot_js_file():
-    """
-    The generated JS file requires some light patching for integration.
-    """
-
-    source = "\n".join(
-        [
-            "/* This Source Code Form is subject to the terms of the Mozilla Public",
-            " * License, v. 2.0. If a copy of the MPL was not distributed with this",
-            " * file, You can obtain one at http://mozilla.org/MPL/2.0/. */",
-            "",
-            "function loadBergamot(Module) {",
-            "",
-        ]
-    )
-
-    with open(JS_PATH, "r", encoding="utf8") as file:
-        for line in file.readlines():
-            source += "  " + line
-
-    source += "  return Module;\n}"
-
-    # Use the Module's printing.
-    source = source.replace("console.log(", "Module.print(")
-
-    # Add some instrumentation to the module's memory size.
-    source = source.replace(
-        "function updateGlobalBufferAndViews(buf) {",
-        """
-        function updateGlobalBufferAndViews(buf) {
-          const mb = (buf.byteLength / 1_000_000).toFixed();
-          Module.print(
-            `Growing wasm buffer to ${mb}MB (${buf.byteLength} bytes).`
-          );
-    """,
-    )
-
-    print("\n Formatting the final bergamot file")
-    # Create the file outside of this directory so it's not ignored by eslint.
-    temp_path = os.path.join(DIR_PATH, "../temp-bergamot.js")
-    with open(temp_path, "w", encoding="utf8") as file:
-        file.write(source)
-
-    subprocess.run(
-        f"./mach eslint --fix {temp_path}",
-        cwd=ROOT_PATH,
-        check=True,
-        shell=True,
-        capture_output=True,
-    )
-
-    print(f"\n Writing out final bergamot file: {FINAL_JS_PATH}")
-    shutil.move(temp_path, FINAL_JS_PATH)
-
-
 def main():
     args: ArgNamespace = parser.parse_args()
 
     if not os.path.exists(THIRD_PARTY_PATH):
         os.mkdir(THIRD_PARTY_PATH)
 
+    ensure_docker()
+
     install_and_activate_emscripten(args)
-    install_bergamot()
+
     build_bergamot(args)
-    write_final_bergamot_js_file()
 
 
 if __name__ == "__main__":
